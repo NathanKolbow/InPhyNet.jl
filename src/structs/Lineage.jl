cc563c0dc486610d5c5b735f97659cf07f0f5230
Nathan Kolbow <nkolbow@wisc.edu>
2023-09-14 16:11:21 -0500

    Conditioning on coalescences working as expected and tested; added framework for cond. on retics
    Former-commit-id: 983759f6796d668c52dcd84549118049b36ea89a [formerly 90ea4b0713454b291dafd651b3896a56edbafe5f]
    Former-commit-id: a532eea008317427e0732d0698544ae659bc5a36 [formerly b899e16be8206e292c10e3b9bb8916ebd519097a]
    Former-commit-id: b57287c7c9e1012193ca61bab4762c65d9d5923b
    Former-commit-id: 90a0d6e18f0c0482cfe455e0a7aebd920f3072af

--- src/structs/Lineage.jl
+++ src/structs/Lineage.jl
@@ -6,6 +6,9 @@ struct Lineage
     lineage::BitArray
     
     # Constructors
+    function Lineage(i::Real, ntaxa::Real)
+        Lineage([i], ntaxa)
+    end
     function Lineage(members::AbstractVector{<:Real}, ntaxa::Real)
         lineage = BitArray([i in members for i=1:ntaxa])
         new(lineage)
@@ -14,8 +17,14 @@ struct Lineage
         # ntaxa(l1) == ntaxa(l2) || error("l1 and l2 have different lengths.")
         new(l1.lineage .|| l2.lineage)
     end
+    function Lineage()
+        new([])
+    end
 end
 
 # Helper methods
 ntaxa(lineage::Lineage) = length(lineage.lineage)
 Base.coalesce(l1::Lineage, l2::Lineage) = Lineage(l1, l2)
+Base.coalesce(ls::AbstractVector{Lineage}) = reduce(coalesce, ls)
+
+const LDict = Dict{PhyloNetworks.Node, Union{LineageNode, Nothing}}
