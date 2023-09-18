1d5ab4fdc125743dc4452cf36003497d40ef41b5
NathanKolbow <nathankolbow@gmail.com>
2023-09-18 11:43:13 -0500

    Pretty print LineageNode & working towards conditioning on retics

--- src/algos/ManipHelpers.jl
+++ src/algos/ManipHelpers.jl
@@ -3,12 +3,53 @@ import DataStructures: Queue
 
 function copyldictcontents!(oldnet::HybridNetwork, newnet::HybridNetwork, ldict::LDict)
     for (newnode, oldnode) in zip(newnet.node, oldnet.node)
-        lineagedict[newnode] = lineagedict[oldnode]
+        ldict[newnode] = ldict[oldnode]
     end
 end
 
 # Splits the given reticulation into 2 tree-like edges
 # Creates a deep copy of the net in the process
-function splitreticulation(net::HybridNetwork, retic::PhyloNetworks.Node, leftline::LineageNode, rightline::LineageNode, ldict::LDict)
+# `retic` is the hybrid node that 2 edges point into
+#
+# "left" and "right" don't actually hold any meaning here,
+# they are just a useful naming convention to keep things clear
+function splitreticulation!(net::HybridNetwork, retic::PhyloNetworks.Node, leftline::LineageNode, rightline::LineageNode, ldict::LDict)
+    println()
+    println("Net address: "*repr(UInt64(pointer_from_objref(net))))
+    println("retic address: "*repr(UInt64(pointer_from_objref(retic))))
+    println()
+    leftedge = getparentedge(retic)
+    rightedge = getparentedgeminor(retic)
 
+    retic.hybrid = false
+    retic.gammaz = -1.
+
+    # TOOD: WARNING: MAY NOT BE UPDATING ALL NECESSARY ATTRIBUTES TO MAINTAIN
+    # TOPOLOGY
+    retic.name = "SPLIT-"*retic.name
+    newleftnode = deepcopy(retic)
+    newrightnode = retic
+
+    replacenodeinedge!(leftedge, retic, newleftnode)
+    # replacenodeinedge!(rightedge, retic, newrightnode)
+
+    removehybridreference!(net, retic)
+
+    if nlineages(leftline) == 0
+        # remove the redundant node
+    elseif nlineages(rightline) == 0
+        # remove the redundant node
+    end
+
+    ldict[newleftnode] = leftline
+    ldict[newrightnode] = rightline
+
+    return net
 end
+
+
+function replacenodeinedge!(edge::PhyloNetworks.EdgeT{PhyloNetworks.Node}, oldnode::PhyloNetworks.Node, newnode::PhyloNetworks.Node)
+    edge.node[findfirst(edge.node .== [oldnode])] = newnode
+end
+
+removehybridreference!(net::HybridNetwork, retic::PhyloNetworks.Node) = filter!(s -> s ≠ retic, net.hybrid)
