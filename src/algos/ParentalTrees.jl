22d95bfc1126a25eb967dd272f82433d58d43bff
Nathan Kolbow <nkolbow@wisc.edu>
2023-09-29 14:14:28 -0500

    Implemented probabilities for parental trees
    Former-commit-id: 94889e66a507d734ff57d616a5af93db9c470f0a

--- src/algos/ParentalTrees.jl
+++ src/algos/ParentalTrees.jl
@@ -8,6 +8,11 @@
 # !! TREES MAY NOT BE UNIQUE !!
 function getparentaltrees(net::HybridNetwork; safe::Bool=true)
     if safe net = deepcopy(net) end
+    _overwritemissinggammas!(net)
+    _overwritemissingbranchlengths!(net)
+
+    # Save original leaf labels
+    leaflabs = [leaf.name for leaf in net.leaf]
 
     # NETWORK PRE-PROCESSING
     ldict = LDict()   # nodes & nets will be deep copied, 
@@ -16,45 +21,54 @@ function getparentaltrees(net::HybridNetwork; safe::Bool=true)
     for node in net.node
         if node.leaf
             ldict[node] = LineageNode(i)
+            node.name = string(i)
             i += 1
         else
             ldict[node] = nothing
         end
     end
 
-    return _getparentaltrees(net, ldict), ldict
+    ipt = IPT(net)
+    ptrees = _getparentaltrees(ipt, ldict)
+
+    # TODO: Condense non-unique parental trees
+
+    # TODO: put the original leaf labels back on the trees
+
+    return ptrees, ldict
 end
 
-function _getparentaltrees(initnet::HybridNetwork, lineagedict::LDict)
-    workingset = Queue{HybridNetwork}()
+function _getparentaltrees(initnet::IPT, lineagedict::LDict)
+    workingset = Queue{IPT}()
     enqueue!(workingset, initnet)
-    parentaltrees = Vector{HybridNetwork}()
+    parentaltrees = Vector{IPT}()
 
     while !isempty(workingset)
-        net = dequeue!(workingset)
+        ipt = dequeue!(workingset)
+        net = top(ipt)
 
         # 0. No more hybrids? This net is done!
         if length(net.hybrid) == 0
-            push!(parentaltrees, net)
+            push!(parentaltrees, ipt)
             continue
         end
 
         # 1. Find a hybrid node with no other hybrids below it
         node, nodeidx = _getnexthybrid(net)
-        divisions = Vector{HybridNetwork}()
+        divisions = Vector{IPT}()
 
         # 2a. If there is already a LineageNode associated with this node,
         #     then we are ready to condition on the reticulation
         if lineagedict[node] !== nothing
             print("Conditioning on reticulations - ")
-            divisions = _conditiononreticulation(net, node, lineagedict)
+            divisions = _conditiononreticulation(ipt, node, lineagedict)
             println("done.")
 
         # 2b. If there is not a LineageNode associated with thisnode,
         #     we must first condition on the coalescent events
         else
             print("Conditioning on coalescences - ")
-            divisions = _conditiononcoalescences(net, node, lineagedict)
+            divisions = _conditiononcoalescences(ipt, node, lineagedict)
             println("done.")
         end
 
@@ -63,6 +77,49 @@ function _getparentaltrees(initnet::HybridNetwork, lineagedict::LDict)
     end
 
     return parentaltrees
+end
+
+# Overwrites any missing/unspecified gammas for hybrids in `net`
+function _overwritemissinggammas!(net::HybridNetwork)
+    invalidgamma(edge::Edge) = edge.gamma == -1. || edge.gamma > 1. || edge.gamma < 0.
+    
+    warn = false
+    for hyb in net.hybrid
+        major = getparentedge(hyb)
+        minor = getparentedgeminor(hyb)
+        if invalidgamma(major)
+            warn = true
+            if invalidgamma(minor)
+                major.gamma = 0.5
+                minor.gamma = 0.5
+            else
+                major.gamma = 1 - minor.gamma
+            end
+        elseif invalidgamma(minor)
+            warn = true
+            minor.gamma = 1 - major.gamma
+        end
+    end
+
+    if warn @warn "One or more invalid gammas found and replaced in original topology." end
+end
+
+function _overwritemissingbranchlengths!(net::HybridNetwork; replacement::Real=1.)
+    warn = false
+    for edge in net.edge
+        if edge.length < 0.
+            warn = true
+            edge.length = replacement
+        end
+    end
+
+    if warn @warn "One or more branch lengths were unspecified and have been replaced with 1.0." end
+end
+
+# Takes an incomplete parental tree and expands its stored LineageNode attributes
+# into a complete parental tree (returned as a `HybridNetwork` object)
+function _expandIPT(tree::IPT, ldict::LDict)
+
 end
 
 # DOES NOT WORK AS WRITTEN
@@ -103,26 +160,38 @@ function _condensedivisions(divisions::Vector{HybridNetwork}, nodes::Vector{Node
 end
 
 # Conditions the given network on the given reticulation
-function _conditiononreticulation(net::HybridNetwork, hyb::Node, lineagedict::LDict)
+function _conditiononreticulation(ipt::IPT, hyb::Node, lineagedict::LDict)
     # If everything is moving as expected, the children should be exclusively
     # one leaf node (signifying a single lineage) or one LineageNode
+    net = top(ipt)
     child = lineagedict[hyb]   # this will throw an error if more than 1 child exists
     hybidx = findfirst(net.node .== [hyb])
 
-    retlist = Vector{HybridNetwork}()
+    retlist = Vector{IPT}()
     for set in powerset([1:nlineages(child);])
         # `set` contains the indices of the children that go up the "left" reticulation
         # while its `setdiff` is those the go up the "right" reticulation
-        leftline = LineageNode(lineages(child)[set])
-        rightline = LineageNode(lineages(child)[setdiff(1:nlineages(child), set)])
+        majorline = LineageNode(lineages(child)[set])
+        minorline = LineageNode(lineages(child)[setdiff(1:nlineages(child), set)])
+
+        # Probability of the given split:
+        #   γ: left inheritance probability
+        #   nmaj: number of lineages going left
+        #   nmin: number of lineages going right
+        #   P(split) = γ^nmaj * (1-γ)^nmin
+        γ = getparentedge(hyb).gamma
+        nmin = length(set)
+        tot = nlineages(child)  # nmaj = tot-nmin
+        splitprob = γ^(tot-nmin) * (1-γ)^nmin
+        newprob = prob(ipt) * splitprob
 
         newnet = deepcopy(net)
         newhyb = newnet.node[hybidx]
         copyldictcontents!(net, newnet, lineagedict)
 
         # Split the reticulation into 2 tree-like splits
-        splitreticulation!(newnet, newhyb, leftline, rightline, lineagedict)
-        push!(retlist, newnet)
+        splitreticulation!(newnet, newhyb, majorline, minorline, lineagedict)
+        push!(retlist, IPT(newnet, newprob))
     end
 
     # # Condense down to unique parental trees
@@ -131,25 +200,28 @@ function _conditiononreticulation(net::HybridNetwork, hyb::Node, lineagedict::LD
 end
 
 # Conditions the given network on the various coalescent possibilities below `node`
-function _conditiononcoalescences(net::HybridNetwork, node::PhyloNetworks.ANode, lineagedict::LDict)
-    if lineagedict[node] !== nothing return net end    # this means we'll never hit leaves
+function _conditiononcoalescences(ipt::IPT, node::Node, lineagedict::LDict)
+    net = top(ipt)
+    if lineagedict[node] !== nothing return ipt end    # this means we'll never hit leaves
     
     children = getchildren(node)
     length(children) <= 2 || error("Polytomies are not accounted for. Found "*string(length(children))*" children of node #"*string(node.number))
 
     # Check that the children are good; if not, recurse
-    nets = Vector{HybridNetwork}([net])
+    ipts = Vector{IPT}([ipt])
     if lineagedict[children[1]] === nothing
-        nets = reduce(vcat, [_conditiononcoalescences(net, children[1], lineagedict) for net in nets])
+        ipts = reduce(vcat, [_conditiononcoalescences(ipt, children[1], lineagedict) for ipt in ipts])
     end
     if length(children) > 1 && lineagedict[children[2]] === nothing
-        nets = reduce(vcat, [_conditiononcoalescences(net, children[2], lineagedict) for net in nets])
+        ipts = reduce(vcat, [_conditiononcoalescences(ipt, children[2], lineagedict) for ipt in ipts])
     end
 
     # In the simplest case, this only has 1 net and `node` is above 2 leaves
-    retlist = Vector{HybridNetwork}()
-    markednodes = Vector{Node}()    # used for condensing later
-    for tempnet in nets
+    retlist = Vector{IPT}()
+    # markednodes = Vector{Node}()    # used for condensing later
+    for tempipt in ipts
+        tempnet = top(tempipt)
+
         # when we copy nets, we need to somehow make sure that lower down references are not lost
         tempnodeidx = findfirst([n.number == node.number for n in tempnet.node])
         tempnode = tempnet.node[tempnodeidx]
@@ -159,21 +231,23 @@ function _conditiononcoalescences(net::HybridNetwork, node::PhyloNetworks.ANode,
         # From that perspective, all the lineages in each of the children nodes have _separate_
         # chances to merge while moving up their branch. So, the resultant node `tempnode`
         # potentially has any pairing of `opts1` and `opts2`
-        opts1 = getcoalescentcombos(lineagedict[children[1]])
+        opts1, probs1 = getcoalescentcombos(lineagedict[children[1]], getparentedge(children[1]).length)
         if length(children) > 1
-            opts2 = getcoalescentcombos(lineagedict[children[2]])
+            opts2, probs2 = getcoalescentcombos(lineagedict[children[2]], getparentedge(children[2]).length)
         else
             opts2 = [LineageNode()]
+            probs2 = [1]
         end
         
-        for (i, opti) in enumerate(opts1)
-            for (j, optj) in enumerate(opts2)
+        for (i, (opti, probi)) in enumerate(zip(opts1, probs1))
+            for (j, (optj, probj)) in enumerate(zip(opts2, probs2))
                 newnet = deepcopy(tempnet)
                 copyldictcontents!(tempnet, newnet, lineagedict)
+                newprob = prob(tempipt) * probi * probj
 
                 lineagedict[newnet.node[tempnodeidx]] = LineageNode(opti, optj)
-                push!(retlist, newnet)
-                push!(markednodes, newnet.node[tempnodeidx])
+                push!(retlist, IPT(newnet, newprob))
+                # push!(markednodes, newnet.node[tempnodeidx])
             end
         end
     end
@@ -185,9 +259,9 @@ end
 
 # Gets all the _deep_ children of `hyb`. I.e., not the nodes immediately proceeding it,
 # but all of the furthest tips that it can reach (not including any intermediate nodes)
-function _getchildrendeep(hyb::PhyloNetworks.ANode)
+function _getchildrendeep(hyb::Node)
     children = getchildren(hyb)
-    returnlist = Vector{PhyloNetworks.ANode}([child for child in children if typeof(child) <: LineageNode || child.leaf])
+    returnlist = Vector{Node}([child for child in children if typeof(child) <: LineageNode || child.leaf])
     for child in setdiff(children, returnlist)
         returnlist = vcat(returnlist, _getchildrendeep(child))
     end
@@ -207,7 +281,7 @@ function _getnexthybrid(net::HybridNetwork)
 end
 
 # Checks whether there are any hybrid nodes below `hyb` in the network
-function _noreticsbelow(node::PhyloNetworks.ANode)
+function _noreticsbelow(node::Node)
     if node.leaf return true end
 
     children = getchildren(node)
