8f1f083ef6e6991bfcaac93e3890cde7373103de
Nathan Kolbow <nkolbow@wisc.edu>
2023-09-29 14:14:28 -0500

    Implemented probabilities for parental trees
    Former-commit-id: 352bf89bd7910c156ba16d69421ac9e3a7f1272c [formerly a9b5dc2013556b7320073d32d5e3c8e0d40ba9a0]
    Former-commit-id: ba79478b9455e483330585b8830af092b794de2f [formerly 101ea76010b80b0f9710e73edfed4e13e578f8a4]
    Former-commit-id: a0efbbfa4eef4a207d65c2772d171d390aad641b

--- src/main.jl
+++ src/main.jl
@@ -1,12 +1,16 @@
 include("imports.jl")
 include("constants.jl")
+include("structs/InterimParentalTree.jl")
 include("structs/Lineage.jl")
 include("structs/LineageNode.jl")
+include("algos/proba.jl")
+include("algos/coalescent.jl")
 include("algos/ParentalTrees.jl")
 include("algos/ManipHelpers.jl")
 
 export Lineage,
     LineageNode,
+    InterimParentalTree, IPT,
     nlineages,
     lineages,
     getParentalTrees,
