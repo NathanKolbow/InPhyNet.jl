2232211675101d6bcbe6a3e04d3004062c4b8ec2
Nathan Kolbow <nkolbow@wisc.edu>
2023-09-29 20:10:53 -0500

    Implemented N-O = 3 coalescent probability
    Former-commit-id: 75417e9409b9d4bf5b1b32707881bf5dc5641926 [formerly ae096c6758d539b3b87ebb2a6c2061737aaa1bfc]
    Former-commit-id: 84433e29a3d511d5613be1f637427f256c590546

--- src/algos/proba.jl
+++ src/algos/proba.jl
@@ -38,7 +38,7 @@ function _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
     N >= O > 0 || error("`N` must be >= `O` and both must be greater than 0")
 
     # Pre-computed probas
-    # functions for all outcomes with N=3: https://www.desmos.com/calculator/c3cnhucqwr
+    # functions for all outcomes with N=3: https://www.desmos.com/calculator/atrnmeryug
     if N == O
         return exp(-binomial(N,2)*bl)
     elseif N - O == 1
@@ -52,10 +52,29 @@ function _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
         c3 = binomial(O, 2)
 
         # TODO: simplify the computation below
-        return (((exp(bl*c1)-1)*c2-c1*exp(bl*c1))*exp(bl*c2)+c1*exp(bl*c1))*exp(-bl*c2-bl*c1) / (c1*c2*(c2-c1))
+        return ((c2-c1)*exp(c2*bl+c1*bl)+((c3-c2)*exp(c2*bl)+(c1-c3)*exp(c1*bl))*exp(c3*bl))*exp(-(c1+c2+c3)*bl) / ((c2-c1)*(c3^2-(c1+c2)*c3+c1*c2))
+        # return (c1*exp(c2*bl+c1*bl)+(((c3-c2)*exp(c1*bl)-c3+c2)*exp(c2*bl)-c1*exp(c1*bl))*exp(c3*bl))*exp(-(c1+c2+c3)*bl) / (c1*(c2-c1)*(c3-c2))
+
+        # Only correct for N=3
+        # return (((exp(bl*c1)-1)*c2-c1*exp(bl*c1))*exp(bl*c2)+c1*exp(bl*c1))*exp(-bl*c2-bl*c1) / (c1*c2*(c2-c1))
+    elseif N - O == 3
+        c1 = binomial(N, 2)
+        c2 = binomial(N-1, 2)
+        c3 = binomial(N-2, 2)
+        c4 = binomial(O, 2)
+
+        num = (((((exp(bl*c2)-exp(bl*c1))*c3-c2*exp(bl*c2)+c1*exp(bl*c1))*exp(bl*c3)+(exp(bl*c1)*c2-c1*exp(bl*c1))*exp(bl*c2))*c4^2+(((exp(bl*c1)-exp(bl*c2))*c3^2+c2^2*exp(bl*c2)-c1^2*exp(bl*c1))*exp(bl*c3)+(c1^2*exp(bl*c1)-exp(bl*c1)*c2^2)*exp(bl*c2))*c4+((c2*exp(bl*c2)-c1*exp(bl*c1))*c3^2+(c1^2*exp(bl*c1)-c2^2*exp(bl*c2))*c3)*exp(bl*c3)+(c1*exp(bl*c1)*c2^2-c1^2*exp(bl*c1)*c2)*exp(bl*c2))*exp(bl*c4)+((c1*exp(bl*c1)-exp(bl*c1)*c2)*exp(bl*c2)*c3^2+(exp(bl*c1)*c2^2-c1^2*exp(bl*c1))*exp(bl*c2)*c3+(c1^2*exp(bl*c1)*c2-c1*exp(bl*c1)*c2^2)*exp(bl*c2))*exp(bl*c3))*exp(-(c1+c2+c3+c4)*bl)
+        denom = (c2-c1)*(c3^2-(c1+c2)*c3+c1*c2)*(c4^3-(c1+c2+c3)*c4^2+((c1+c2)*c3+c1*c2)*c4-c1*c2*c3)
+
+        return num/denom
     end
 
-    error("Coalescent probabilities only implemented up to an in-out difference of 2; received (N,O) := ("*string(N)*","*string(O)*")")
+    error("Coalescent probabilities only implemented up to an in-out difference of 3; received (N,O) := ("*string(N)*","*string(O)*")")
 end
 
 _calculatecoalescentprobability(3, 3, 1.) + 3*_calculatecoalescentprobability(3, 2, 1.) + 3*_calculatecoalescentprobability(3, 1, 1.)
+
+_calculatecoalescentprobability(4, 4, 1.) + 
+    binomial(4, 2)*_calculatecoalescentprobability(4, 3, 1.) +
+    binomial(4, 2)*binomial(4-1,2)*_calculatecoalescentprobability(4, 2, 1.) +
+    binomial(4, 2)*binomial(4-1,2)*binomial(4-2,2)*_calculatecoalescentprobability(4, 1, 1.)
