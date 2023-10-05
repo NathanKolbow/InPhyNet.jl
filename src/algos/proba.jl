13a2b2439cb04da3a12ca881782f8d420a497caf
Nathan Kolbow <nkolbow@wisc.edu>
2023-10-05 17:11:37 -0500

    Touched up computation logging

--- src/algos/proba.jl
+++ src/algos/proba.jl
@@ -9,8 +9,7 @@
 
 # TODO: instead of setting as globals, keep these variables in the main algo and pass them to this function
 # TODO: make sure all inputs to `exp` functions and all else are `BigFloat`s
-global const savedprobs = Ref{Dict{Tuple{Real, Real, Real}, BigFloat}}(Dict{Tuple{Real, Real, Real}, BigFloat}())
-global const savedcomps = Ref{Int64}(0)
+
 """
     _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
 
@@ -26,8 +25,7 @@ they must be castable to integer.
 # Return Value
 Real value in (0, 1]; the probability of the coalescence under the given parameters happening.
 """
-function _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
-    # println("("*string(N)*","*string(O)*","*string(bl)*")")
+function _calculatecoalescentprobability(N::Real, O::Real, bl::Real; complog::Union{CompDict,Nothing}=nothing)
     # Make sure `N` and `O` are castable as `Real`
     try 
         N = Int64(N)
@@ -42,24 +40,21 @@ function _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
     end
 
     N >= O > 0 || error("`N` must be >= `O` and both must be greater than 0")
-    savedict = savedprobs[]
 
-    # if haskey(savedict, (N, O, bl))
-    #     savedcomps[] += 1
-    #     return savedict[N, O, bl]
-    # end
+    if complog !== nothing && haskey(complog, (N, O, bl))
+        return complog[N, O, bl]
+    end
 
     # Pre-computed probas
     # functions for all outcomes with N=4: https://www.desmos.com/calculator/0x4jmv0muv
+    retval = 0.
     if N == O
-        savedict[N, O, bl] = exp(-binomial(N,2)*bl)
-        return exp(-binomial(N,2)*bl)
+        retval = exp(-binomial(N,2)*bl)
     elseif N - O == 1
         c1 = binomial(N, 2)
         c2 = binomial(O, 2)
-        savedict[N, O, bl] = (exp(-bl*c1) - exp(-bl*c2)) / (c2 - c1)
 
-        return (exp(-bl*c1) - exp(-bl*c2)) / (c2 - c1)
+        retval = (exp(-bl*c1) - exp(-bl*c2)) / (c2 - c1)
     elseif N - O == 2
         c1 = binomial(N, 2)
         c2 = binomial(N-1, 2)
@@ -68,22 +63,22 @@ function _calculatecoalescentprobability(N::Real, O::Real, bl::Real)
         # TODO: simplify the computation below
         num = ((c2-c1)*exp(c2*bl+c1*bl)+((c3-c2)*exp(c2*bl)+(c1-c3)*exp(c1*bl))*exp(c3*bl))*exp(-(c1+c2+c3)*bl)
         denom = (c2-c1)*(c3^2-(c1+c2)*c3+c1*c2)
-        savedict[N, O, bl] = num/denom
 
-        return num/denom
+        retval = num/denom
     elseif N - O == 3
         c1 = binomial(N, 2)
         c2 = binomial(N-1, 2)
         c3 = binomial(N-2, 2)
         c4 = binomial(O, 2)
 
-        # TODO: this computation is wrong
         num = (((((exp(bl*c2)-exp(bl*c1))*c3-c2*exp(bl*c2)+c1*exp(bl*c1))*exp(bl*c3)+(exp(bl*c1)*c2-c1*exp(bl*c1))*exp(bl*c2))*c4^2+(((exp(bl*c1)-exp(bl*c2))*c3^2+c2^2*exp(bl*c2)-c1^2*exp(bl*c1))*exp(bl*c3)+(c1^2*exp(bl*c1)-exp(bl*c1)*c2^2)*exp(bl*c2))*c4+((c2*exp(bl*c2)-c1*exp(bl*c1))*c3^2+(c1^2*exp(bl*c1)-c2^2*exp(bl*c2))*c3)*exp(bl*c3)+(c1*exp(bl*c1)*c2^2-c1^2*exp(bl*c1)*c2)*exp(bl*c2))*exp(bl*c4)+((c1*exp(bl*c1)-exp(bl*c1)*c2)*exp(bl*c2)*c3^2+(exp(bl*c1)*c2^2-c1^2*exp(bl*c1))*exp(bl*c2)*c3+(c1^2*exp(bl*c1)*c2-c1*exp(bl*c1)*c2^2)*exp(bl*c2))*exp(bl*c3))*exp(-(c1+c2+c3+c4)*bl)
         denom = (c2-c1)*(c3^2-(c1+c2)*c3+c1*c2)*(c4^3-(c1+c2+c3)*c4^2+((c1+c2)*c3+c1*c2)*c4-c1*c2*c3)
 
-        savedict[N, O, bl] = num/denom
-        return num/denom
+        retval = num/denom
+    else
+        error("Coalescent probabilities only implemented up to an in-out difference of 3; received (N,O) := ("*string(N)*","*string(O)*")")
     end
 
-    error("Coalescent probabilities only implemented up to an in-out difference of 3; received (N,O) := ("*string(N)*","*string(O)*")")
+    if complog !== nothing complog[N, O, bl] = retval end
+    return retval
 end
