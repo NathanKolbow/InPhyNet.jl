340f98b5cc314b8e3fce6fc754a6c686544814bf
NathanKolbow <nathankolbow@gmail.com>
2023-10-09 12:13:01 -0500

    coalescent combo debugging
    Former-commit-id: 23b9f9f744bcd3b4684847b80e84208e9202d73e [formerly f76402c21875668bac4ebea0334fc5048f626d5f]
    Former-commit-id: df29cc5d97bacdb339b8b8a0de6c333dd6aa4496 [formerly 1b0344fa66200a9803877572075fb578fc8b39c7]
    Former-commit-id: bc243da647432ed3a61e375529ba79ded49c6557

--- src/algos/arbitrary-proba.jl
+++ src/algos/arbitrary-proba.jl
@@ -120,6 +120,44 @@ evaluate(f::foneobj, bl::Real) = evaluate(f, BigFloat(bl))
 
 
 
-# Testing - below should be equal
-# g(4, 2, 1.)
-# _calculatecoalescentprobability(4, 2, 1.)
+# Testing
+"""
+Tests the above probabilities for `N` input lineages
+and branch length `bl`.
+"""
+function probatest(N::Int64, bl::Real)
+    probsum = 0.
+    for nout=1:N
+        prob = _calculatecoalescentprobability(N, nout, bl)
+        mult = prod([binomial(i, 2) for i=(nout+1):N])
+        probsum += mult * prob
+    end
+    abs(probsum - 1.) < 1e-12 || error("probsum: "*string(probsum))
+end
+
+probatest(4, 1.) || error("failed")
+probatest(5, 1.) || error("failed")
+probatest(6, 1.) || error("failed")
+probatest(7, 1.) || error("failed")
+probatest(8, 1.) || error("failed")
+
+"""
+Tests the above probabilities for `N` input lineages
+and branch length `bl`.
+"""
+function probatoptest(N::Int64, bl::Real)
+    N > 0 || error("N>0 required.")
+    bl = BigFloat(bl)
+    lnode = LineageNode([Lineage(i) for i=1:N])
+    combos, probs = getcoalescentcombos(lnode, bl)
+
+    println(combos)
+    println([round(Float64(p), digits=5) for p in probs])
+    abs(sum(probs) - BigFloat(1.)) < 1e-12 || error("sum(probs) = "*string(sum(probs)))
+end
+
+probatoptest(3, 1.)
+probatoptest(4, 1.)
+probatoptest(5, 1.)
+probatoptest(6, 1.)
+probatoptest(7, 1.)
--- src/algos/coalescent.jl
+++ src/algos/coalescent.jl
@@ -46,8 +46,8 @@ function getcoalescentcombos(l::LineageNode, bl::Real; complog::Union{CompDict,N
         # We need to make sure we're counting things like [1, 2], [3, 4] twice
         # or [1, 2], [3, 4], [5, 6] six times b/c order of coalescence is not
         # taken into account in the above `prob` calculation
-        totalcoalevents = sum([length(set) >= 2 for set in part])
-        prob *= factorial(totalcoalevents)
+        totalcoalevents = _findtotalcoaleventsinpart(part)
+        prob *= totalcoalevents
         
         push!(problist, prob)
 
@@ -62,15 +62,53 @@ function getcoalescentcombos(l::LineageNode, bl::Real; complog::Union{CompDict,N
     return (lineagelist, problist)
 end
 
-"""
-Tests the above probabilities for `N` input lineages
-and branch length `bl`.
-"""
-function probatest(N::Int64, bl::Real)
-    N > 0 || error("N>0 required.")
-    bl = BigFloat(bl)
-    lnode = LineageNode([Lineage(i) for i=1:N])
-    _, probs = getcoalescentcombos(lnode, bl)
-
-    abs(sum(probs) - BigFloat(1.)) < 1e-12 || error("sum(probs) = "*string(sum(probs)))
+function _findtotalcoaleventsinpart(part)
+    totalevents = 1
+    g2sets = 0
+    for set in part
+        totalevents *= _findtotalcoaleventsinpartrecur(set)
+        g2sets += length(set) >= 2
+    end
+    println(max(totalevents, 1) * max(g2sets, 1))
+    return max(totalevents, 1) * max(g2sets, 1)
 end
+
+function _findtotalcoaleventsinpartrecur(part)
+    if typeof(part) <: Lineage return 1 end
+
+    totalevents = 0
+    nestedevents = 1
+    for set in part
+        if typeof(set) <: AbstractArray && length(set) >= 2
+            totalevents += 1
+            nestedevents *= max(_findtotalcoaleventsinpartrecur(set), 1)
+        end
+    end
+
+    if totalevents == 0
+        return binomial(length(part), 2)
+    else
+        return totalevents * nestedevents
+    end
+end
+
+
+bigl = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+_findtotalcoaleventsinpart(bigl)
+_findtotalcoaleventsinpart([[1, 2], [3]])
+_findtotalcoaleventsinpart([[1, 2], [3, 4]])
+_findtotalcoaleventsinpart([[1, 2], [3, 4], [5, 6]])
+_findtotalcoaleventsinpart([[1, 2], [3, 4], [5, 6, 7]])
+_findtotalcoaleventsinpart([[1, 2], [3, 4, 8], [5, 6, 7]])
+_findtotalcoaleventsinpart([[1, 2], 3, 4])
+_findtotalcoaleventsinpart([[1, [2, 3]], 3])
+_findtotalcoaleventsinpart([1, 2, [3, 4]])
+_findtotalcoaleventsinpart([1, 2, 3, 4])
+
+# Next on the TODO list: run `probatoptest(4, 1.)` in arbitrary-proba.jl and look for incorrect things in the probs output
+
+# Current state of errors: before, we realized the [1, 2], [3, 4] was getting counted once when it needed to get counted
+# twice to account for the fact that either [1, 2] or [3, 4] could coalesce first. Now, we aren't double counting that in
+# Base.coalesce in Lineage.jl by push!ing twice anymore, we're accounting for it in _findtotalcoaleventsinpart
+# This isn't working as expected because some things still aren't getting counted the correct number of times
+# but it is HORRIBLY UNCLEAR WHY THAT IS
