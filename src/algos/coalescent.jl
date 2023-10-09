9862043b91fde2749951a9fa990d44a8f36c68ca
Nathan Kolbow <nkolbow@wisc.edu>
2023-10-09 16:02:54 -0500

    Added binomial precomputations

--- src/algos/coalescent.jl
+++ src/algos/coalescent.jl
@@ -7,27 +7,33 @@
 # returns a tuple (A, B)
 # A: list of all possible coalescent combos
 # B: the probability associated with the corresponding entry of A
-function getcoalescentcombos(l::LineageNode, bl::Real; complog::Union{CompDict,Nothing}=nothing)
-    parts = partitions(lineages(l))
+"""
+    getcoalescentcombos(l::LineageNode, bl::Real; complog::Union{CompDict,Nothing}=nothing)
 
+Gets all possible coalescent combinations for the lineages in `l` and their associated 
+probabilities, including ones where lineages in `l` do not coalesce.
+
+# Arguments
+- `l`: the relevant `LineageNode`
+- `bl`: branch length that coalescing is happening in; used for probabilities
+- `complog`: used to store computations so that the same work is not done multiple times
+"""
+function getcoalescentcombos(l::LineageNode, bl::Real, binoms::AbstractArray; complog::Union{CompDict,Nothing}=nothing)
+    parts = partitions(lineages(l))
     N = nlineages(l)
 
+    # We are going to be dividing by these values later
+    counts = zeros(Int64, N)
+    for part in parts counts[length(part)] += 1 end
+
+    incompleteidxs = Vector{Int64}()
     lineagelist = Vector{LineageNode}()
     problist = Vector{BigFloat}()
-    for part in parts
+
+    for (i, part) in enumerate(parts)
         # Calculate the probability of this outcome `part`
-        prob = _calculatetotalcoalescentprobability(N, length(part), bl, complog=complog)
-        prob /= sum([length(p) == length(part) for p in parts])
+        push!(problist, _calculatetotalcoalescentprobability(N, length(part), bl, binoms, complog=complog) / counts[length(part)])
 
-        # We need to make sure we're counting things like [1, 2], [3, 4] twice
-        # or [1, 2], [3, 4], [5, 6] six times b/c order of coalescence is not
-        # taken into account in the above `prob` calculation
-        # totalcoalevents = _findtotalcoaleventsinpart(part)
-        # prob *= totalcoalevents
-        
-        push!(problist, prob)
-
-        # If we didn't get caught in the above loop, that means this partition is good to be pushed
         templin = LineageNode()
         for lin in part
             push!(lineages(templin), Lineage(lin)) 
