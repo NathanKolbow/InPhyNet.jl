a6dc15ffe26ef284aece4db5daae6eb373c014ba
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-06 15:06:12 -0600

    Added boiler plate files for subset decomposition and merging algo
    Former-commit-id: 942d0e31e26e15d135ed4a85060ec3bf5d45f2a2

---
+++ src/algos/njmerge/subsetdecomp.jl
@@ -0,0 +1,11 @@
+# Source for decomposing full species set into subsets
+
+const Quartet = Nothing
+
+"""
+## Arguments:
+- T: the quickly estimated, rough  species tree
+"""
+function decomposeFromQuartets(T::HybridNetwork, R::AbstractVector{Quartet})
+    
+end
