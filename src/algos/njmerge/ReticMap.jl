4981d429b583c03ab49187f4a6498eecef463139
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-10 12:00:17 -0600

    First tree-like merging test added and working PERFECTLY!

--- src/algos/njmerge/ReticMap.jl
+++ src/algos/njmerge/ReticMap.jl
@@ -1,15 +1,14 @@
 import Base: getindex, setindex!
 
-const EdgeOrNA = Union{Edge, Nothing}
 struct ReticMap
-    map::Dict{Edge, Tuple{EdgeOrNA, EdgeOrNA}}
+    map::Dict{Edge, Set{Edge}}
     function ReticMap(constraints::Vector{HybridNetwork})
         # map all existing reticulations in `constraints` to `(nothing, nothing)`
         d = Dict()
         for net in constraints
             for e in net.edge
                 if e.hybrid && !e.isMajor
-                    d[e] = (nothing, nothing)
+                    d[e] = Set{Edge}()
                 end
             end
         end
@@ -17,12 +16,4 @@ struct ReticMap
     end
 end
 
-Base.getindex(r::ReticMap, e::Edge) = r[e]
-Base.getindex(r::ReticMap, e::Edge, dir::AbstractString) = ifelse(dir == "from", r[e][1], r[e][2])
-function Base.setindex!(r::ReticMap, e::Edge, dir::AbstractString, newe::Edge)
-    if dir == "fron"
-        return r[e] = (newe, r[e][2])
-    else
-        return r[e] = (r[e][1], newe)
-    end
-end
+logretic(r::ReticMap, constraintedge::Edge, subnetedge::Edge) = push!(r[constraintedge], subnetedge)
