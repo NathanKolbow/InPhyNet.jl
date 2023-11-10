d341a97b07564bfe504b87fdf230545f8555364b
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-10 12:00:17 -0600

    First tree-like merging test added and working PERFECTLY!
    Former-commit-id: efbef8190800de4bc85d1fbd85c4c5b0a2f1f68b [formerly 0ad581876dd9cfebe34295eff8b2aa694bdf94e0]
    Former-commit-id: 2b55163452172a737c1024a4345dc216a264f00c [formerly 723c65d1aadae599e374ce855ae65e31c3f696fc]
    Former-commit-id: 12d77d57b1774d8622c4b00b47e027253e277152
    Former-commit-id: 2121852f8817f070c7cfb1ded120c04711fbd3ce

---
+++ src/tests/netnjmerge/mergealgo.jl
@@ -0,0 +1,32 @@
+include("../../main.jl")
+
+# Test w/ a tree example
+T = readTopology("((((A,B),C),(D,E)),(((F,G),(H,I)),((J,(K,L)),((M,N),O))));")
+constraints = [
+    readTopology("(((A,B),C),(H,I));"),
+    readTopology("((D,E),((M,N),O));"),
+    readTopology("((F,G),(J,(K,L)));")
+]
+D = Matrix{Float64}([
+     0.  1. 2.  4.  4.  8.  8.  8.  8.  8.  9.  9.  9.  9.  8.;
+    -1.  0. 2.  4.  4.  8.  8.  8.  8.  8.  9.  9.  9.  9.  8.;
+    -1. -1. 0.  3.  3.  6.  6.  6.  6.  7.  8.  8.  8.  8.  7.;
+    -1. -1. -1. 0.  1.  7.  7.  7.  7.  8.  9.  9.  9.  9.  8.;
+    -1. -1. -1. -1. 0.  7.  7.  7.  7.  8.  9.  9.  9.  9.  8.;
+    -1. -1. -1. -1. -1. 0.  1.  3.  3.  6.  7.  7.  7.  7.  6.;
+    -1. -1. -1. -1. -1. -1. 0.  3.  3.  6.  7.  7.  7.  7.  6.;
+    -1. -1. -1. -1. -1. -1. -1. 0.  1.  6.  7.  7.  7.  7.  6.;
+    -1. -1. -1. -1. -1. -1. -1. -1. 0.  6.  7.  7.  7.  7.  6.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.  2.  2.  4.  4.  3.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.  1.  5.  5.  4.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.  5.  5.  4.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.  1.  2.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.  2.;
+    -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. -1. 0.;
+])
+for i=1:size(D, 1) for j=(i+1):size(D,1) D[j,i] = D[i,j] end end
+mergednet = netnj!(D, constraints, names=T.names)
+mergednet = HybridNetwork(mergednet.nodes, mergednet.edges)
+mergednet.root = mergednet.numNodes
+writeTopology(mergednet)
+hardwiredClusterDistance(T, mergednet, true)
