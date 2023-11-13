5a3a12f31a58a0e17d6f52bd2eed2a9f75a07c7e
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-13 14:44:38 -0600

    network merging works for one reticulation orientation
    Former-commit-id: 590b5d69a0bfcb16be82a8b618df4f2a69705988 [formerly 5014cb736b61a6b61c40991de59889b08c880c0b]
    Former-commit-id: 42d9d365cba0c8136ec8549d6c0b928c3531fa65

--- src/algos/njmerge/ReticMap.jl
+++ src/algos/njmerge/ReticMap.jl
@@ -1,14 +1,15 @@
 import Base: getindex, setindex!
 
+const EdgeOrNA = Union{Edge, Nothing}
 struct ReticMap
-    map::Dict{Edge, Vector{Edge}}
+    map::Dict{Edge, Vector{EdgeOrNA}}
     function ReticMap(constraints::Vector{HybridNetwork})
         # map all existing reticulations in `constraints` to `(nothing, nothing)`
         d = Dict()
         for net in constraints
             for e in net.edge
                 if e.hybrid && !e.isMajor
-                    d[e] = Vector{Edge}()
+                    d[e] = Vector{EdgeOrNA}([nothing, nothing])
                 end
             end
         end
@@ -16,4 +17,10 @@ struct ReticMap
     end
 end
 
-logretic!(r::ReticMap, constraintedge::Edge, subnetedge::Edge) = push!(r.map[constraintedge], subnetedge)
+function logretic!(r::ReticMap, constraintedge::Edge, subnetedge::Edge, fromorto::String)
+    if fromorto == "from"
+        r.map[constraintedge][1] = subnetedge
+    else
+        r.map[constraintedge][2] = subnetedge
+    end
+end
