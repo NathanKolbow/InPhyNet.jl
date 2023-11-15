be633eac4ef789b0e448bb506b1e2e166e412634
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-15 15:52:56 -0600

    moved dependencies to imports.jl
    Former-commit-id: a206aad45402dde18e16edc80a196562e0b3eb19
    Former-commit-id: c77720b78203f0ab2f12f293048397a6a806594a

--- src/imports.jl
+++ src/imports.jl
@@ -2,7 +2,11 @@ using PhyloNetworks, DataStructures, DataFrames, CSV, Graphs
 
 import PhyloNetworks: deleteNode!, deleteEdge!, addhybridedge!, fuseedgesat!
 import Combinatorics: combinations
-import Base: ==, *, -
+import Base: ==, *, -, names, getindex, setindex!
 import Combinatorics: partitions, powerset
 import Test: @warn
 import Graphs: SimpleGraph, a_star, add_edge!
+
+
+# Just for dev, to be removed later
+using Plots, GraphRecipes, PhyloCoalSimulations, PhyloPlots
