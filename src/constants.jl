cfddfeef59c085cb1adfaf961c4b668ce680f868
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-14 19:26:49 -0600

    implemented MSCquartets interface, subset decomposition algorithm, working on fixing a bug when updating constraints from beginning constraint network ((t11,#H3),((t12,(t13,t14)))#H3);
    Former-commit-id: 744a9fbd007796e19d359c97b0c50da37f6f62d8 [formerly 404d4c0e6a7e94e8ad8b1a8e62b46fb48284fc8c] [formerly df77f8687aeb4925683ee81719b2f4deb54426e0] [formerly ede821a2b19f8bfcbe46343219d74310f49eaed3] [formerly 3ff3597f95a2f989365578c6dd00d038839218dc [formerly b9a0e168d6a299f20a7156fab7e0a0e0d1574e11]] [formerly e2ccb2ef55ab817d1902308f2fff9f0444ea28d6 [formerly a903fa7abec347f2cccc25fcc3fb0548f70f4c86]]
    Former-commit-id: 4e03ed0440b29f00f02b86d90a551781d7d4a5fe
    Former-commit-id: 892588e4b012648b2bdfc6ed8df983c262d0070a [formerly 4d4190cf340419b9efb9bb86bfd96c381d1fc066]
    Former-commit-id: 53ac8aac00dc2f14a62e0e46080ad7515ebdaa74 [formerly f364ce74ef98ad40c1473493c0f42750f5fae953]
    Former-commit-id: 6fbc55bc4ee11fdace617e0724ab598c9f3453fa
    Former-commit-id: e7379c96136805cd23cfa634126531990d8467cc
    Former-commit-id: 5c8ff9084e39a233837576c305379dac1161db69

--- src/constants.jl
+++ src/constants.jl
@@ -2,3 +2,6 @@ const Node = PhyloNetworks.Node
 const Edge = PhyloNetworks.EdgeT{Node}
 const CompDict = Dict{Tuple{Real, Real, Real}, BigFloat}
 const Fxn = Function
+const Quartet = Vector{String}
+const QuartetVector = Vector{Quartet}
+const ListOfNames = AbstractVector{<:AbstractString}
