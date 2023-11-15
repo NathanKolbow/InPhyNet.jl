9d15659c48a9d25a6c2aa42891386b9fad450c5b
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-14 19:26:49 -0600

    implemented MSCquartets interface, subset decomposition algorithm, working on fixing a bug when updating constraints from beginning constraint network ((t11,#H3),((t12,(t13,t14)))#H3);
    Former-commit-id: 889fda57ed44aa8de10c36cf7d1d52ce070c338c [formerly 1956a82b9b842cdd9d6fa2827f3f92df89889984]
    Former-commit-id: d793e29d9d4e1d2bbffdfbf22e5663b036e5f876 [formerly d389c246c05b434a322c40552a551be9fc5b435d]
    Former-commit-id: da931b7b8fbb588a4bf2022ee3f8d4b97764e1aa
    Former-commit-id: b79cbc1138ac6c02305d34e4cf1400513d9e80a4
    Former-commit-id: 7b1a177235c2b16ec3fe761f2183804f25aa5ce3

--- src/algos/njmerge/mscquartets-graphs.jl
+++ src/algos/njmerge/mscquartets-graphs.jl
@@ -1,64 +1,9 @@
 using CSV, DataFrames, Graphs, Combinatorics, Plots
-
-################################################################
-# These functions are actually related to subset decomposition #
-################################################################
-
-function requiredsubsets(df, cutoff)
-    minsubset = minimumuniquesubset(df, cutoff=cutoff)
-    return kconnected(df, minsubset, k=4, cutoff=0.01)
-end
-
-function minimumuniquesubset(df; cutoff=0.01)
-    df = deepcopy(df)
-    N = sum(df[:,"HBp_T3"] .<= cutoff)
-    subset = []
-    namelist = names(df)[2:(findfirst(names(df) .== "12|34")-1)]
-
-    while sum(df[:,"HBp_T3"] .<= cutoff) > 0
-        reject = df[:,"HBp_T3"] .<= cutoff
-        counts = [sum(df[:,name] .== 1 .&& reject) for name in namelist if name in names(df)]
-        
-        removeidx = argmax(counts)
-        myname = namelist[removeidx]
-
-        numremoved = sum(df[:,myname] .== 1 .&& reject)
-        removerows = df[:,myname] .== 1
-        df = df[.!removerows,:]
-
-        push!(subset, namelist[removeidx])
-        deleteat!(namelist, removeidx)
-        deleteat!(counts, removeidx)
-    end
-
-    return subset
-end
-
-function kconnected(df, minsubset; k::Int64=4, cutoff=0.01)
-    k > 1 && k <= 4 || error("k must be in [2, 4]")
-    kconnected = []
-    namelist = names(df)
-    namelist = namelist[2:(findfirst(namelist .== "12|34")-1)]
-
-    for name in minsubset
-        tempdf = view(df, df[:,name] .== 1 .&& df[:,"HBp_T3"] .<= cutoff, :)
-        counts = [sum(tempdf[:,partnername] .== 1) for partnername in namelist]
-        topk = sortperm(counts, rev=true)[1:k]
-        push!(kconnected, namelist[topk])
-    end
-    return kconnected
-end
-
-################################################################
-#                                                              #
-################################################################
-
 # MSCquartets implemented in Julia
 
 
 
 # definitely have to read through things somewhat carefully, not just skim
-
 function hbpgraph(hbpdf, cutoff)
     namelist = names(hbpdf)
     namelist = namelist[2:(findfirst(namelist .== "12|34")-1)]
@@ -77,6 +22,7 @@ function hbpgraph(hbpdf, cutoff)
     end
     return graph
 end
+
 
 # constraints[1] from mergealgo.jl; very connected, as expected
 hbpdf = CSV.read("/Users/nkolbow/repos/network-merging/src/tests/netnjmerge/10000-constraints1.hbptab", DataFrame)
