a72aae58830b12ab16f7dca7fb49f115edd55a13
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-14 19:26:49 -0600

    implemented MSCquartets interface, subset decomposition algorithm, working on fixing a bug when updating constraints from beginning constraint network ((t11,#H3),((t12,(t13,t14)))#H3);

--- src/imports.jl
+++ src/imports.jl
@@ -1,5 +1,6 @@
-using PhyloNetworks
-using DataStructures
+using PhyloNetworks, DataStructures, DataFrames, CSV, Graphs
+
+import PhyloNetworks: deleteNode!, deleteEdge!, addhybridedge!, fuseedgesat!
 import Combinatorics: combinations
 import Base: ==, *, -
 import Combinatorics: partitions, powerset
