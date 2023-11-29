8b8b420e96429b1d6266eb058657d2784e59dd33
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-29 16:01:06 -0600

    added pipeline boilerplates for simulations
    Former-commit-id: cf4e313d72f1ea3eb45adb08255de0a288fb5ffa [formerly 489c516d5bb48a9517844b8768308beaff64cda5] [formerly 10e91a4a6ee623d1ef8978f923abfbd473d963ee] [formerly 05f2a4475e1c839965cab0be5d98300e5e5b4827] [formerly 4506425a37dc69971073741d40cbdf342f066330 [formerly d96299f9a4c7843d54df573f44f8c9be7643f06f]] [formerly ea7c18e7048e174b3def924e3ace92705cc8b685 [formerly 74732bf3b7091bf6f828a8d42d453fde0e0d0a9e]]
    Former-commit-id: d39e4dad1a9bce0914c370154815f27074d9f633
    Former-commit-id: 46a1df41e8bc777b67ca66ccd527d3638aaf4834 [formerly fc15b457b6d725d1bead61f3bb8935c1199011d6]
    Former-commit-id: f045d19e346aae031e34204a067705f44d366d99 [formerly 85e051d1bb14214b5b952416ff68f24b73d9941c]
    Former-commit-id: 815a9c17b666021c1e0bf9a12829e97a0a7f7227
    Former-commit-id: 820e83310c28295d8adb1bc3138503eeffea2e53
    Former-commit-id: ba5a7028d09f5c53d1a4871a5f26d689c38183e5

---
+++ simulations/pipelines.jl
@@ -0,0 +1,41 @@
+using Pkg
+Pkg.activate(Base.source_dir())
+
+using NetMerge
+
+
+function runGroundTruthPipeline(truenet::HybridNetwork, constraints::Vector{HybridNetwork})
+    # 1. Calculate distance matrix
+    D, namelist = majorinternodedistance(truenet)
+
+    # 2. Merge
+    return netnj(D, constraints, namelist=namelist)
+end
+
+
+function runEstGtPipeline(estgts::Vector{HybridNetwork})
+    # 1. MSCquartets
+    error("MSCquartets not ported into pure julia yet")
+    # hybsubsets, treesubset = ...
+
+    # 2. Calculate distance matrix
+    D, namelist = calculateAGID(estgts)
+
+    # 3. SNaQ
+    q, t = countquartetsintrees(estgts, showprogressbar=false)
+    startingtree = PhyloNetworks.nj!(deepcopy(D), deepcopy(namelist))
+
+    constraints = Array{HybridNetwork}(undef, length(hybsubsets))
+    for (j, hybsub) in enumerate(hybusbsets)
+        # Filter out all quartets that are not exclusively composed of taxa in `hybsub`
+        temptaxonnumbers = [i for i in 1:length(t) if t[i] in hybsub]
+        tempq = view(q, [i for i in 1:length(q) if all([number in temptaxonnumbers for number in q[i].taxonnumber])])
+        tempdf = readTableCF(writeTableCF(tempq, t))
+
+        # Estimate the network
+        constraints[j] = snaq!(startingtree, tempdf, hmax=Int64(ceil(length(hybsub) / 3)), runs=10)
+    end
+
+    # 4. Merge
+    return netnj(D, constraints, namelist=namelist)
+end
