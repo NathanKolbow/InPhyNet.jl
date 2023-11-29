1b49988f6399040705095d4baceab45ba0641b73
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-29 13:02:20 -0600

    reorganized source code
    Former-commit-id: 235226e3f4b486eb6510fd83e2e32ea3fd094a72 [formerly 62c61380599dc6423002449a921f891cf3ef7d31] [formerly 6b794f5124f7263b080c3b6bfaf02c4e4b9339ed] [formerly 0838a1eec1a39084b45e1ed2322a8673588299e3] [formerly 0c37d3907b68636cc30caa2f9639e014f177aa11 [formerly 0c2d0bc2ac05d72cf0baae580c597976914b98b3]] [formerly b4bb045bda005bc9e57d3e2f8b68c3820e74624b [formerly e91abe4b06819a0c551cdf91ce22bfae2895f353]]
    Former-commit-id: 4f3b5bf00a532942c9dca08d48d284457f29e8c0
    Former-commit-id: 7ab56f10ddf124a112f2ebd951da4cf92a38504e [formerly 54a9c1c32740ebf4ed0366c87594bb302249d2e7]
    Former-commit-id: bcb6377e3c4eb4595d744b2e7d424b8f82629b31 [formerly 1aa04b5f5ab219e9ce219b32829d377779d88cdb]
    Former-commit-id: 87c85918ae84da2ef24c754722526a3cb1b1dc72
    Former-commit-id: fa123bb962a3950fb82f2c2ff0e46add8eb214a0
    Former-commit-id: f6fa3bd58d998b0e41b5a453cf27f88d296d6a82

---
+++ src/ptrees/structs/Lineage.jl
@@ -0,0 +1,92 @@
+# Contains info for which species/taxa/etc. this lineage is an ancestor for
+# while preserving coalescent order; e.g. [[3, 2], [1]]
+# means that this is an ancestor of A, B, and C, and that BC must be
+# together, coalescing before A
+mutable struct Lineage
+    # Fields
+    lineage::Vector
+    
+    # Constructors
+    Lineage(i::Real) = new([i])
+    Lineage(l1::Lineage, l2::Lineage) = new(condense([l1.lineage, l2.lineage]))
+    function Lineage(ls::Vector{Lineage}) 
+        condensed = condense(ls)
+        if typeof(condensed) <: Vector
+            new(condensed)
+        end
+        new([condensed])
+    end
+    Lineage() = new([])
+end
+
+# Helper methods
+ntaxa(lineage::Lineage) = length(lineage.lineage)
+lineage(l::Lineage) = l.lineage
+Base.coalesce(l1::Lineage, l2::Lineage) = Lineage(l1, l2)
+
+# Condenses the `lineage` field as such:
+# [[[[[1]]], [[[[3]]]]]] --> [1, 3]
+function condense(ls)
+    if typeof(ls) <: Lineage 
+        return condense(ls.lineage)
+    elseif typeof(ls) <: Vector && length(ls) == 1
+        if typeof(ls[1]) <: Lineage
+            return condense(ls[1])
+        end
+        return ls[1]
+    elseif typeof(ls) <: Real
+        return ls
+    end
+    return [condense(l) for l in ls]
+end
+
+# Returns all possible combinations of `ls` *in which all of the lineages
+# end up coalescing*
+#
+# Order matters, so we can't just go from [1, 2, 3] -> [123]
+# we need to instead go [1, 2, 3] -> [[1, 2], 3], [1, [2, 3]], [[1, 3], 2]
+function Base.coalesce(ls::AbstractVector{Lineage})
+    if length(ls) == 1 return ls end
+    if length(ls) == 2 return [Lineage(ls[1], ls[2])] end
+
+    choose2sets = partitions(ls, 2)
+    retlist = []
+    for set in choose2sets
+        subsetsi = coalesce(set[1])
+        subsetsj = coalesce(set[2])
+
+        for opti in subsetsi
+            for optj in subsetsj
+                push!(retlist, Lineage(opti, optj))
+            end
+        end
+    end
+    return retlist
+end
+
+function Base.:(==)(l1::Lineage, l2::Lineage)
+    if length(lineage(l1)) != length(lineage(l2)) return false end
+    if length(lineage(l1)) == 1 return lineage(l1)[1] == lineage(l2)[1] end
+
+    l2idxs = collect(1:length(lineage(l2)))
+    for i in eachindex(lineage(l1))
+        foundequality = -1
+        for j in eachindex(lineage(l2))
+            if lineage(l1)[i] == lineage(l2)[j]
+                foundequality = j
+                break
+            end
+        end
+        if foundequality == -1 return false end
+        deleteat!(l2idxs, foundequality)
+    end
+
+    return true
+end
+
+l1 = Lineage(Lineage(Lineage(1), Lineage(2)), Lineage(3))
+l2 = Lineage(Lineage(3), Lineage(Lineage(1), Lineage(2))) 
+l3 = Lineage(Lineage(1), Lineage(Lineage(3), Lineage(2)))
+l1 == l2 || error("Lineage equality test failed.")
+l1 != l3 || error("Lineage equality test failed.")
+l2 != l3 || error("Lineage equality test failed.")
