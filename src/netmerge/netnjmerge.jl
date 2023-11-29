d00012d9b74e775a65d435c8db805a02b919c7de
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-29 13:02:20 -0600

    reorganized source code
    Former-commit-id: 15dd016244ce5cdb7b23e96461eab24a31e27042 [formerly ee69c3d8545772b76c256b596472cb4798b1b19b] [formerly 99c88b976925eaa67003a49e0319b3cfdaee4b10 [formerly 47b530e38efb665f09aa01f780fbb0868ef930a8]]
    Former-commit-id: 727d318f714adf66f3a0be6b539d5d920fb03024
    Former-commit-id: 1d7437201467fb104b5b4a7dbf7fc9e14901ab56 [formerly 5a468796478c34c3300f72d590b885519ba9da5c]
    Former-commit-id: 5e913734edf1ac23382ef1bad19abbad0f00d0b5

--- src/netmerge/netnjmerge.jl
+++ src/netmerge/netnjmerge.jl
@@ -1,13 +1,22 @@
-# Source for the network version of NJ merge
-function netnj(D::Matrix{Float64}, constraints::Vector{HybridNetwork})
-    return netnj!(
-        deepcopy(D),
-        deepcopy(constraints)
-    )
+# Author: Nathan Kolbow
+# Hosted at https://github.com/NathanKolbow/network-merging
+#
+# Source code for main components of the network merging algorithm.
+
+"""
+    netnj(D::Matrix{Float64}, constraints::Vector{HybridNetwork})
+
+Creates a super-network based on the constraint trees/networks in `constraints` and
+distance matrix `D`.
+
+# Arguments
+- D: distance matrix relating pairs of taxa. This can be generated from estimated gene trees with 
+"""
+function netnj(D::Matrix{Float64}, constraints::Vector{HybridNetwork}, namelist::AbstractVector{<:AbstractString})
+    return netnj!(deepcopy(D), deepcopy(constraints), namelist)
 end
 
-function netnj!(D::Matrix{Float64}, constraints::Vector{HybridNetwork};
-    namelist::AbstractVector{<:AbstractString}=String[])
+function netnj!(D::Matrix{Float64}, constraints::Vector{HybridNetwork}, namelist::AbstractVector{<:AbstractString})
     
     PhyloNetworks.check_distance_matrix(D)
     check_constraints(constraints, false)
@@ -655,93 +664,3 @@ function getsiblingcandidates(leaf::Node)
 end
 
 getnodes(n::Node) = reduce(vcat, [[child for child in e.node if child != n] for e in n.edge], init=Vector{Node}())
-
-function Graph(net::HybridNetwork; includeminoredges::Bool=true, alwaysinclude::Union{Edge,Nothing}=nothing)
-    graph = SimpleGraph(net.numNodes)
-    for edge in net.edge
-        if !includeminoredges && edge.hybrid && !edge.isMajor && edge != alwaysinclude continue end
-        nidx1 = findfirst(net.node .== [edge.node[1]])
-        nidx2 = findfirst(net.node .== [edge.node[2]])
-
-        if nidx1 !== nothing && nidx2 !== nothing
-            add_edge!(graph, nidx1, nidx2)  # edges are undirected by default, don't need to add twice
-        end
-    end
-    return graph
-end
-
-
-"""
-
-Helper function that removes redundant edges in the graph that exist
-when some hybrids are pruned from the graph.
-"""
-function removeredundantedges!(graph::SimpleGraph; keeproot::Union{Nothing,HybridNetwork}=nothing)
-    rootidx = -1
-    if keeproot !== nothing
-        rootidx = keeproot.root
-    end
-
-    redundantidxs = []
-    for (i, adjvec) in enumerate(graph.fadjlist)
-        if length(adjvec) == 2 && i != rootidx
-            push!(redundantidxs, i)
-        end
-    end
-
-    if length(redundantidxs) == 0 return end
-    for idx in redundantidxs
-        from = graph.fadjlist[idx][1]
-        to = graph.fadjlist[idx][2]
-        
-        vecreplace!(graph.fadjlist[from], idx, to)
-        vecreplace!(graph.fadjlist[to], idx, from)
-        graph.fadjlist[idx] = []
-    end
-    return redundantidxs
-end
-
-
-"""
-
-Helper function that adds edges coming out from the root in `net` back into `graph`.
-"""
-function addrootedge!(graph::SimpleGraph, net::HybridNetwork)
-    totaladded = 0
-    rootnode = net.node[net.root]
-    rootedges = rootnode.edge
-
-    dst1 = ifelse(rootedges[1].node[1] == rootnode, rootedges[1].node[2], rootedges[1].node[1])
-    dst2 = ifelse(rootedges[2].node[1] == rootnode, rootedges[2].node[2], rootedges[2].node[1])
-    idxdst1 = findfirst(net.node .== [dst1])
-    idxdst2 = findfirst(net.node .== [dst2])
-    idxsrc = findfirst(net.node .== [rootnode])
-
-    remove_edge!(graph, idxdst1, idxdst2)
-    remove_edge!(graph, idxsrc, idxdst1)
-    remove_edge!(graph, idxsrc, idxdst2)
-
-    for edge in net.node[net.root].edge
-        nidx1 = findfirst(net.node .== [edge.node[1]])
-        nidx2 = findfirst(net.node .== [edge.node[2]])
-        
-        if !(nidx2 in graph.fadjlist[nidx1])
-            totaladded += 1
-            add_edge!(graph, nidx1, nidx2)
-        end
-    end
-    return totaladded
-end
-
-function remove_edge!(graph::SimpleGraph, idx1::Int64, idx2::Int64)
-    remidxs = findall(graph.fadjlist[idx1] .== idx2)
-    deleteat!(graph.fadjlist[idx1], remidxs)
-    
-    remidxs = findall(graph.fadjlist[idx2] .== idx1)
-    deleteat!(graph.fadjlist[idx2], remidxs)
-end
-
-# Test for `findsiblingpairs`
-# nn = readTopology("((A,(B,#H1)),((C)#H1,D));")
-# pairs = findsiblingpairs(nn)
-# length(pairs) == 3 || error("test failed in netnjmerge.jl")
