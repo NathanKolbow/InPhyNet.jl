6eb51c11febada6bd1125fb0c5bdad094902cee0
Nathan Kolbow <nkolbow@wisc.edu>
2023-11-29 14:16:54 -0600

    added docs
    Former-commit-id: 67e0e5f60fe53e8239cd70dfcb587203c619c83c [formerly 97e1924b560d5800db0a8a85ed63a3d52173f6dd] [formerly b110577c35607dfa41978fa8c1518c8f3e02095a] [formerly f1ae116df25da7612e67c49d5fa811d58bd289b1 [formerly 112db7a978b08ffdf8c002a259d09efcc4631c24]] [formerly 97255e7cb089e8496e51e655b1d71c0445b2c559 [formerly 61907767e3e8be9560b872ad733cc1fe898a878e]]
    Former-commit-id: 0fbf4ee05cdae2cb4986bf73600ca6d83e2de303
    Former-commit-id: 653c221ac3dda243a5ace5b5f7919f474b8ef4fd [formerly 7ad497ccc5a6d8b110b1345dcf62a407d3bfed33]
    Former-commit-id: e3b936fb0697c58b4d8bddac598a6acaa8ae6975 [formerly 809d9bd0d09a397f4a499e2158388a92e9587532]
    Former-commit-id: 391275e0804e3b8197d4ffa897793ab98cf5adb8
    Former-commit-id: 2ecc1bd0f2a327de8db70f58b6b62664678ab897

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, NetMerge
+using DocThemeIndigo
+
+indigo = DocThemeIndigo.install(NetMerge)
+makedocs(
+    modules=[NetMerge],
+    sitename="NetMerge.jl",
+    authors="Nathan Kolbow",
+    format=Documenter.HTML(;
+        prettyurls=true,
+        assets=String[indigo]
+    ),
+    clean=true,
+    repo="NetMerge",
+    draft=true,
+    pages=[
+        "Introduction" => "index.md",
+        "Guide" => [
+            "Using estimated gene trees" => "guide.md"
+        ],
+        "Documentation" => "documentation.md"
+    ],
+    checkdocs=:exports
+)
