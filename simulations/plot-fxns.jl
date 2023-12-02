734db00bdeb9162e61073772b11e1ad6f44dbac6
NathanKolbow <nathankolbow@gmail.com>
2023-12-02 13:36:26 -0600

    Plotting fxns, pipeline fxns, plot image, sim results
    Former-commit-id: f0472610d36ab74ce1623cb9026892bef2b1a7a7

--- simulations/plot-fxns.jl
+++ simulations/plot-fxns.jl
@@ -1,18 +1,22 @@
-using Plots
+using Plots, StatsPlots
 
-function plotNNIErrors(mergedists, constraintdiffs)
-    y = mergedists
-    x = sum(constraintdiffs, dims=1)[1,:]
+function plotNNIErrors(mergedists, constraintdiffs, add=false)
+    y = Vector{Float64}(mergedists)
+    x = Vector{Float64}(sum(constraintdiffs, dims=1)[1,:])
     
     neutral = y .== x
     worse = y .> x
     better = y .< x
 
     # Jitter the points
-    y = y .+ rand(length(dists)) / 2
-    x = x .+ rand(length(dists)) / 2
+    y = y .+ rand(length(y)) / 2
+    x = x .+ rand(length(x)) / 2
 
-    scatter(x[neutral], y[neutral], xlabel="Sum of induced errors", ylabel="Merged net error", labels="No difference", color="black")
+    if !add
+        scatter(x[neutral], y[neutral], xlabel="Sum of errors induced by NNI moves", ylabel="Merged net error", labels="No difference", color="black")
+    else
+        scatter!(x[neutral], y[neutral], xlabel="Sum of errors induced by NNI moves", ylabel="Merged net error", labels="No difference", color="black")
+    end
     scatter!(x[worse], y[worse], labels="Worse than induced", color="red")
     if any(better)
         scatter!(x[better], y[better], labels="Better than induced", color="green")
@@ -20,3 +24,20 @@ function plotNNIErrors(mergedists, constraintdiffs)
     plot!([0, maximum(y)], [0, maximum(y)], primary=false)
 end
 
+function histNNIErrors(mergedists, constraintdiffs, add=false; kwargs...)
+    y = Vector{Float64}(mergedists)
+    x = sum(constraintdiffs, dims=1)[1,:]
+    y .+= rand(length(y)) / 2
+
+    if !add
+        violin(x, y,
+            xlabel="Sum of errors induced by NNI moves",
+            ylabel="Merged net error"; kwargs...
+        )
+    else
+        violin!(x, y,
+            xlabel="Sum of errors induced by NNI moves",
+            ylabel="Merged net error"; kwargs...
+        )
+    end
+end
