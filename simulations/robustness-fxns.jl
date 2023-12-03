eacc9aaefc4fe1c1578e2100fa20108903f9e7ce
NathanKolbow <nathankolbow@gmail.com>
2023-12-03 14:27:14 -0600

    more functions for testing robustness
    Former-commit-id: ded1e81fc7e404ca85e529fa8bea358063a8e9a3
    Former-commit-id: 304b9440ef7cda2759ec99a5f905d7b7e6ab0c0c

--- simulations/robustness-fxns.jl
+++ simulations/robustness-fxns.jl
@@ -1,27 +1,13 @@
+using Distributions, Random
+
 function robustNNI(truenet::HybridNetwork, constraints::Vector{HybridNetwork},
     nmoves::Int64; nsim::Int64=100)
 
     dists = Array{Int64}(undef, nsim)
     constraintdiffs = Array{Int64}(undef, 4, nsim)
-    
-
-    # Threads.@threads for i=1:nsim
-    #     truenet, constraints = loadTrueData("n40h4")
-    #     for (j, c) in enumerate(constraints)
-    #         origc = readTopology(writeTopology(c))
-    #         for _=1:nmoves
-    #             e = sample(c.edge, 1, replace=false)[1]
-    #             nni!(c, e)
-    #         end
-    #         constraintdiffs[j, i] = hardwiredClusterDistance(origc, c, false)
-    #     end
-
-    #     mnet = runGroundTruthPipeline(truenet, constraints)
-    #     dists[i] = hardwiredClusterDistance(truenet, mnet, false)
-    # end
 
     Threads.@threads for i=1:nsim
-        newconstraints = [readTopology(writeTopology(c)) for c in constraints]
+        newconstraints = copyConstraints(constraints)
         for (j, (c, newc)) in enumerate(zip(constraints, newconstraints))
             for _=1:nmoves
                 e = sample(newc.edge, 1, replace=false)[1]
@@ -35,3 +21,93 @@ function robustNNI(truenet::HybridNetwork, constraints::Vector{HybridNetwork},
     end
     return dists, constraintdiffs
 end
+
+
+function robustGauss(truenet, constraints; μ::Float64=0., σ::Float64=1., nsim::Int64=100)
+    dists = Array{Int64}(undef, nsim)
+    dists .= -1
+    rgen = Normal(μ, σ)
+
+    Threads.@threads for i=1:nsim
+        constraintscopy = copyConstraints(constraints)
+        D, namelist = majorinternodedistance(truenet)
+        addnoise!(D, rgen)
+        
+        try
+            mnet = netnj(D, constraintscopy, namelist)
+            dists[i] = hardwiredClusterDistance(truenet, mnet, false)
+        catch e
+            # pass
+        end
+    end
+    return dists[dists .!= -1]
+end
+
+function robustUniformProportion(truenet, constraints, p; nsim::Int64=100)
+    dists = Array{Int64}(undef, nsim)
+    dists .= -1
+
+    Threads.@threads for i=1:nsim
+        constraintscopy = copyConstraints(constraints)
+        D, namelist = majorinternodedistance(truenet)
+        addpnoise!(D, p)
+        
+        try
+            mnet = netnj(D, constraintscopy, namelist)
+            dists[i] = hardwiredClusterDistance(truenet, mnet, false)
+        catch e
+            # pass
+        end
+    end
+    return dists[dists .!= -1]
+end
+
+function robustRandD(truenet, constraints; nsim::Int64=100)
+    dists = Array{Int64}(undef, nsim)
+    dists .= -1
+
+    Threads.@threads for i=1:nsim
+        constraintscopy = copyConstraints(constraints)
+        D, namelist = majorinternodedistance(truenet)
+        randomize!(D)
+        
+        try
+            mnet = netnj(D, constraintscopy, namelist)
+            dists[i] = hardwiredClusterDistance(truenet, mnet, false)
+        catch e
+            # pass
+        end
+    end
+    return dists[dists .!= -1]
+end
+
+@inline function randomize!(D)
+    n = size(D)[1]
+    for i=1:n
+        for j=(i+1):n
+            D[i, j] = D[j, i] = rand()
+        end
+    end
+end
+
+@inline function addpnoise!(D, p)
+    n = size(D)[1]
+    for i=1:n
+        for j=(i+1):n
+            val = D[i, j]
+            D[i, j] = D[j, i] = rand(Uniform((1-p)*val, (1+p)*val))
+        end
+    end
+end
+
+@inline function addnoise!(D, rgen)
+    n = size(D)[1]
+    for i=1:n
+        for j=(i+1):n
+            D[i, j] += max(0, rand(rgen))
+            D[j, i] = D[i, j]
+        end
+    end
+end
+
+copyConstraints(cs::Vector{HybridNetwork}) = [readTopology(writeTopology(c)) for c in cs]
