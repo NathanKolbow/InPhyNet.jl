49290fd3cf02b1d821641f7a4ff6d556a7510361
Nathan Kolbow <nkolbow@wisc.edu>
2023-12-06 12:32:59 -0600

    added hierarchical clustering method of subset decomposition
    Former-commit-id: 84b8c761384d3a7791f7eb62f99a640d1bc5d41a
    Former-commit-id: 648d1594647ab27b4c17c7ff43eac85e745adc92

---
+++ src/netmerge/subsetdecomp/nj-deomp.jl
@@ -0,0 +1,32 @@
+using Clustering, DataFrames
+
+
+function njHierarchDecomp(estgts::Vector{HybridNetwork}, maxtax::Int64; clustkwargs...)
+    D, namelist = calculateAGID(estgts)
+    njtree = nj(DataFrame(D, namelist))
+    return njHierarchDecomp(njtree, maxtax; clustkwargs...)
+end
+
+
+function njHierarchDecomp(tree::HybridNetwork, maxtax::Int64; clustkwargs...)
+    D, namelist = internodedistance(tree)
+    hres = hclust(D; clustkwargs...)
+
+    nclust = 1
+    hclusts = nothing
+    maxsize = Inf
+    while maxsize > maxtax
+        nclust += 1
+        hclusts = cutree(hres, k=nclust)
+        maxsize = maximum([sum(hclusts .== i) for i=1:nclust])
+    end
+
+    subsets = []
+    for uqval in unique(hclusts)
+        push!(subsets, namelist[hclusts .== uqval])
+    end
+    if minimum([length(s) for s in subsets]) < 7
+        @warn "WARNING: smallest subset has $(minimum([length(s) for s in subsets])) taxa"
+    end
+    return subsets
+end
