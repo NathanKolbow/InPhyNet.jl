56993c6c0f5971a8c87c05823587b2c004536ec8
Nathan Kolbow <nkolbow@wisc.edu>
2024-01-11 16:48:05 -0600

    added all code except result saving
    Former-commit-id: 94f94fb2ff7b6a0ff4e7505c1fc4fd8a2eb7b216 [formerly 01995134e0561727d12bb110f1359b6607aba62d]
    Former-commit-id: 3ece21b597bc53afa6ec8b2acd88567b56617ec7

--- simulation-study/simulation-scripts/perfect_subsets.jl
+++ simulation-study/simulation-scripts/perfect_subsets.jl
@@ -1,7 +1,31 @@
-include("helpers/pipelines.jl")
-
-if length(ARGS) != 1
-    error("Usage: julia perfect_subsets.jl \"<true network newick>\"")
+# MAKE SURE TO RUN WITH `julia --project -tX ...`
+# TODO: switch up how network newick is passed. passing as a string will probably get too long and cause problems eventuallyget
+if length(ARGS) != 3
+    error("Usage: julia perfect_subsets.jl \"<true network newick>\" <maximum subset size> \"<distance method>\"")
 end
 truenewick = ARGS[1]
+maxsubsetsize = parse(Int64, ARGS[2])
+dmethod = ARGS[3]
 
+include("helpers/robustness-fxns.jl")
+include("helpers/save-results.jl")
+
+# 0. gather ground truth distance matrix
+truenet = readTopology(truenewick)
+
+D, namelist = (nothing, nothing)
+if dmethod == "internode_count"
+    D, namelist = majorinternodedistance(truenet)
+else
+    error("Unrecognized distance method specified.")
+end
+
+# 1. subset decomp w/ SATe-I decomp method
+taxa_subsets = sateIdecomp(majorTree(truenet), maxsubsetsize)
+constraints = pruneTruthFromDecomp(truenet, taxa_subsets)
+
+# 2. run robustness testing
+esterrors, gausserrors, constraintdiffs = monophyleticRobustness(truenet, constraints, D, namelist)
+constraintdiffs = sum(constraintdiffs, dims=1)[1,:]
+
+# 3. save results
