eb025c8119cb786b7915e184f8d8d39e83141220
Nathan Kolbow <nkolbow@wisc.edu>
2024-01-17 16:14:29 -0600

    Simulation scripts and helpers up and running!
    Former-commit-id: c58d01a1e03d5868caee05d4f1032a0b96bd4fae [formerly afc98725f581e5615da443e4d2403c397f8d6986]
    Former-commit-id: 3d0efec254fda4628f0f13554b465379f2dab3f1
    Former-commit-id: e99ab6ba7f3d02ebaad54913f9cff5a12ad50f6c

--- simulation-study/simulation-scripts/helpers/save-results.jl
+++ simulation-study/simulation-scripts/helpers/save-results.jl
@@ -1,2 +1,40 @@
 # Need to map out how the results will be stored first.
-error("Not implemented yet you dingus.")
+include("retic-metric.jl")
+
+using CSV, DataFrames
+
+function savePerfectResults(truenet::HybridNetwork, constraints::Vector{HybridNetwork}, esterrors::Vector{<:Real},
+    gausserrors::Vector{<:Real}, constraintdiffs::Vector{<:Real}, nretics_est::Vector{<:Real})
+
+    # Quick checks for bad input
+    a = length(gausserrors)
+    b = length(esterrors)
+    c = length(constraintdiffs)
+    d = length(nretics_est)
+    a == b == c == d || error("Input vector lengths not identical: $a - $b - $c - $d")
+
+    # Relevant variables
+    nrows = length(gausserrors)
+    output_path = getOutputFilepath(truenet)
+    if !isfile(output_path) touch(output_path) end
+    
+    # Calculate some relevant data points
+    nretics_inside, nretics_outside, nretics_duplicated = calculateReticData(truenet, constraints)
+    constraint_sizes = string([c.numTaxa for c in constraints])
+    constraint_sizes = constraint_sizes[2:(length(constraint_sizes)-1)]
+
+    # Put together and save data
+    df = DataFrame(
+        numtaxa=repeat([truenet.numTaxa], nrows),
+        nretics_true=repeat([truenet.numHybrids], nrows),
+        nretics_est=nretics_est,
+        nretics_inside=repeat([nretics_inside], nrows),
+        nretics_outside=repeat([nretics_outside], nrows),
+        nretics_duplicated=repeat([nretics_duplicated], nrows),
+        constraint_sizes=constraint_sizes,
+        estRFerror=esterrors,
+        gauss_error=gausserrors,
+        constraint_error_sum=constraintdiffs
+    )
+    CSV.write(output_path, df, append=true)
+end
