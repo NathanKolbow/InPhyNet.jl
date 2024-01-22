d03c1352690a1bc1340a9cf6e07018972326945b
NathanKolbow <nathankolbow@gmail.com>
2024-01-22 13:28:47 -0600

    small updates
    Former-commit-id: a81a846c559b29f41ad536fe66ce77d84d9b017a [formerly 42f2e19aba3f166adb5ecea608de57a188084a2a] [formerly d47853a993366b0d0b2f09217c2ec096b40c7b1c] [formerly f44a05e4858c8f91b5d23d3aaa8af6d861a079a8] [formerly d9a80dfa1294b730f032288b2ed1786e3b4598e7 [formerly e8d484d2effc9426dc5d92d3d1aa01b6f710d7a7]] [formerly b19d33cc07e6308ebba07199c48c5c5bca6b00d2 [formerly 5ce45abde111d939f5cef89f31c908cdd708fce7]]
    Former-commit-id: 2593282de577289e4316c2d1e148c44d3d7b20d3
    Former-commit-id: 676ca882769df95ded5d45fb5cdeb93a8edca9b4 [formerly 667f14fcef3a5d87e9cadaacf9a32f4c2344e6a0]
    Former-commit-id: 40020c2909003c512e83fda7950ec31cd3be57af [formerly bed74c8bd666524bbbc8cec9db1e3d1aa1863313]
    Former-commit-id: 5a1407f499a78068bbc01fe0425017d6171bec22
    Former-commit-id: 980e6b52ba62d04699b33c66bf3870cc57158109
    Former-commit-id: 6c7c7f75b96f50b2035ad110377ddc987abdfe2b

--- simulation-study/simulation-scripts/perfect_subsets.jl
+++ simulation-study/simulation-scripts/perfect_subsets.jl
@@ -1,7 +1,10 @@
 # MAKE SURE TO RUN WITH `julia --project -tX ...`
+# if running from a screen session: run from simulation-study/simulation-scripts/
+# - `julia --project=../.. -tX ./perfect_subsets.jl ...`
+
 #error("Double check in Slack how we're choosing the Gaussian standard error and refactor `monophyleticRobustness` to use this schema.")
 if length(ARGS) != 4 && length(ARGS) != 5
-    error("Usage: julia perfect_subsets.jl \"<true network abbreviation>\" <network/replicate number> <maximum subset size> \"<distance method>\" [number of sims]")
+    error("Usage: julia perfect_subsets.jl \"<true network abbreviation>\" <replicate number> <maximum subset size> \"<distance method>\" [number of sims]")
 end
 
 # distance method options:
