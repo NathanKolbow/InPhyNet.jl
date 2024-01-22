f786e02524f47a9354c5d2d31f2c7d1a3104ef39
NathanKolbow <nathankolbow@gmail.com>
2024-01-22 13:28:47 -0600

    small updates
    Former-commit-id: fe6b22a2d923ff1b6bbdb8b244835ae4f790d662
    Former-commit-id: b7e07a4d7e40e2b481b354939abfceed02672c89

--- simulation-study/condor/scripts/perfect_subsets.sh
+++ simulation-study/condor/scripts/perfect_subsets.sh
@@ -5,4 +5,19 @@
 # $4: distance method ("internode_count")
 # $5: number of sims
 
-\"<true network abbreviation>\" <network/replicate number> <maximum subset size> \"<distance method>\" [number of sims]
+netid=$1
+replicate=$2
+maxsubsetsize=$3
+dmethod=$4
+nsim=$5
+nthreads=$6
+
+# Unpack Julia
+tar -xzf julia-1.9.3-linux-x86_64.tar.gz
+tar -xzf netmerge-proj.tar.gz
+export PATH=$PWD/julia-1.9.3/bin:$PATH
+export JULIA_DEPOT_PATH=$PWD/netmerge-proj
+
+# Run network merging
+echo "julia -t${nthreads} --project=netmerge-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}
+julia -t${nthreads} --project=netmerge-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}
