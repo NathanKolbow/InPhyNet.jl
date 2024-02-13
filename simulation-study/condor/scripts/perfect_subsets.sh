12bf3d14bb70df30d033414b6d4118067481353e
Nathan Kolbow <nkolbow@wisc.edu>
2024-02-13 15:08:49 -0600

    refactored NetMerge --> InPhyNet
    Former-commit-id: 1b24422da9460275e89a504b408e26b690d35b0a [formerly ce439a32d97577bfa213639b3085bbde65427f04]
    Former-commit-id: 84d5707a20cda673eaeeab58664b360159bff22e [formerly b4e7066e556bf8716ad8bba7b4647ef849c89f49]
    Former-commit-id: 19d29a32d3a9ed045692b00ff8014d896728563d
    Former-commit-id: 070c6f9feeca88c33c74a69c59e1f5b7633c8a6b
    Former-commit-id: 58d24b31a8c0ba64fb0fe44f19937022b1468737

--- simulation-study/condor/scripts/perfect_subsets.sh
+++ simulation-study/condor/scripts/perfect_subsets.sh
@@ -14,10 +14,10 @@ nthreads=$6
 
 # Unpack Julia
 tar -xzf julia-1.9.3-linux-x86_64.tar.gz
-tar -xzf netmerge-proj.tar.gz
+tar -xzf inphynet-proj.tar.gz
 export PATH=$PWD/julia-1.9.3/bin:$PATH
-export JULIA_DEPOT_PATH=$PWD/netmerge-proj
+export JULIA_DEPOT_PATH=$PWD/inphynet-proj
 
 # Run network merging
-echo "julia -t${nthreads} --project=netmerge-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}
-julia -t${nthreads} --project=netmerge-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}
+echo "julia -t${nthreads} --project=inphynet-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}
+julia -t${nthreads} --project=inphynet-proj ./perfect_subsets.jl ${netid} ${replicate} ${maxsubsetsize} ${dmethod} ${nsim}inphynet
