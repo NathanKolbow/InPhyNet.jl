75dd7677f7ccc5aca0d16df68ce0092451dae324
Nathan Kolbow <nkolbow@wisc.edu>
2024-02-13 15:09:04 -0600

    refactored NetMerge --> InPhyNet
    Former-commit-id: 1b5440c7b3c9e28cd87aa61c5b74f9ea90fee187
    Former-commit-id: 1da08481f9790e35c135c6e6a81de08f4e54f9ed

---
+++ src/inphynet/internodedistance.jl
@@ -0,0 +1,60 @@
+# TODO: change all return values to be NamedArrays instead of matrices and a vector of names
+
+"""
+    majorinternodedistance(N::HybridNetwork)
+
+Calculates internode distances between all pairs of taxa in the major displayed tree
+of network `N`. If `N` is a tree, then internode distances are as expected for a tree.
+"""
+function majorinternodedistance(N::HybridNetwork)
+    return internodedistance(majorTree(N))
+end
+
+
+"""
+    internodedistance(N::HybridNetwork; namelist::Union{Nothing,<:AbstractVector{String}}=nothing)
+
+Calculates internode distances between all pairs of taxa in network `N`.
+"""
+function internodedistance(N::HybridNetwork; namelist::Union{Nothing,<:AbstractVector{String}}=nothing)
+    D = zeros(N.numTaxa, N.numTaxa)
+    if namelist == nothing
+        namelist = [l.name for l in N.leaf]
+    end
+
+    Ngraph = Graph(N)
+    removeredundantedges!(Ngraph)
+    nodelistidx = [findfirst([n.name == name for n in N.node]) for name in namelist]
+
+    for i=1:(N.numTaxa-1)
+        nodenumi = nodelistidx[i]
+        nodei = N.node[nodenumi]
+        for j=(i+1):N.numTaxa
+            nodenumj = nodelistidx[j]
+            nodej = N.node[nodenumj]
+
+            D[i, j] = D[j, i] = length(a_star(Ngraph, nodenumi, nodenumj)) - 1
+        end
+    end
+    return D, namelist
+end
+
+
+"""
+    calculateAGID(Ns::AbstractVector{HybridNetwork})
+
+Calculates the **A**verage **G**ene tree **I**nternode **D**istance for all pairs of taxa
+across all networks in `Ns`.
+
+# Returns
+
+1. AGID matrix
+2. names of taxa corresponding to rows/columns in the AGID matrix
+"""
+function calculateAGID(Ns::AbstractVector{HybridNetwork})
+    D, namelist = internodedistance(Ns[1])
+    for j=2:length(Ns)
+        D .+= internodedistance(Ns[j], namelist=namelist)[1]
+    end
+    return D ./ length(Ns), namelist
+end
---
+++ src/inphynet/structs/SubNet.jl
@@ -0,0 +1,52 @@
+struct SubNet
+    # Fields
+    nodes::Vector{Node}
+    edges::Vector{Edge}
+    linkpoint::Node
+    id::Int64
+
+    # Constructors
+    SubNet() = error("not yet implemented")
+    function SubNet(id, name::AbstractString)
+        taxanode = Node(0, true)
+        taxanode.name = name
+
+        new([taxanode], [], taxanode, id)
+    end
+    SubNet(nodes::Vector{Node}, edges::Vector{Edge}, link::Node, newid::Int64) = new(nodes, edges, link, newid)
+end
+
+
+"""
+
+Merges two unrooted SubNets into a single unrooted SubNet
+Returns the merged subnet AND the edges in the merged net
+    corresponding to where reticulations will be placed if there
+    are corresponding reticulations discovered in the constraints.
+
+IMPORTANT: this function should NOT make copies of edges or
+    nodes, object references are stored and used in
+    the main merging algorithm
+"""
+function mergesubnets!(n1::SubNet, n2::SubNet; newid::Int64=n1.id)
+    newlink = Node(0, false)
+    e1 = connectnodes!(newlink, n1.linkpoint)
+    e2 = connectnodes!(newlink, n2.linkpoint)
+
+    return SubNet([n1.nodes; n2.nodes; newlink], [n1.edges; n2.edges; e1; e2], newlink, newid), e1, e2
+end
+
+
+"""
+Deals with all the overhead of connecting two nodes with an edge.
+Returns the edge used to connect the nodes.
+"""
+function connectnodes!(child::Node, parent::Node)
+    edge = Edge(0, -1.)
+    edge.node = [child, parent]
+    push!(child.edge, edge)
+    push!(parent.edge, edge)
+    edge.containRoot = false
+    edge.isChild1 = true
+    return edge
+end
