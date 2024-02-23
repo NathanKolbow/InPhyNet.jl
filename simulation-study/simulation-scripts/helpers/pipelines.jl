629c480e8b6639a82da5ae71076583842e840611
NathanKolbow <nathankolbow@gmail.com>
2024-02-23 14:52:18 -0600

    Now removing root-retics automatically from simulated data
    Former-commit-id: 09155858759d685aac0698a3543bad43a6042ed3 [formerly 90633009926a84cd8fff838e7948b1e10531aa93] [formerly c8233141264fcbb7c7dbc4044e29c34536f5cd07] [formerly 278ac7facf9fda7f84935136f4d4e3976a3ea151 [formerly 87ef4df004b553b5cf814fa0353ad4ddb1b29696]] [formerly b0b07c65b71bc28c169e9ec579b7c997c7e18fdd [formerly 402e093d91ef6f1818105b2f481dd9b0831023d2]]
    Former-commit-id: 69f859dc0db695598485d9a65950f7fb0ce4d54f
    Former-commit-id: 714d81d0f75d4ec6bf948be64e7b70cac5c28bfe [formerly 37034db5f4932a603ec904d3a89d0a59edbe1842]
    Former-commit-id: 91333f46ae1685d56f448b8629acbf4cbc9ca1a0 [formerly 765e5829a3dfb6f3c29ee189f1c6f7af7ba220d9]
    Former-commit-id: 3b5fbc006f3da83f71d5d9ca0425fdcdfc7639d1
    Former-commit-id: 2e6ec453c40013b58a8be3e003657bbd1a7403be

--- simulation-study/simulation-scripts/helpers/pipelines.jl
+++ simulation-study/simulation-scripts/helpers/pipelines.jl
@@ -25,7 +25,20 @@ function loadPerfectData(netid::String, replicatenum::Int64, maxsize::Int64, dme
 
     constraints = sateIdecomp(majorTree(truenet), maxsize)
     constraints = pruneTruthFromDecomp(truenet, constraints)
+
+    InPhyNet.check_constraints(constraints)
     
+    # If any constraints have root-retics, remove them
+    for c in constraints
+        hybridbools = [edge.hybrid for edge in c.node[c.root].edge]
+        if any(hybridbools)
+            @warn "Fixing root retics."
+            println("Before: $(writeTopology(c))")
+            PhyloNetworks.deletehybridedge!(c, c.node[c.root].edge[hybridbools][1])
+            println("After:  $(writeTopology(c))")
+        end
+    end
+
     D, namelist = (nothing, nothing)
     if dmethod == "internode_count"
         D, namelist = majorinternodedistance(truenet)
