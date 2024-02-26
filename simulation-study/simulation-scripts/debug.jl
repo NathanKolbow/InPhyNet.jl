c0e8f75792d80344812681524487ad9c14599b67
NathanKolbow <nathankolbow@gmail.com>
2024-02-26 12:58:35 -0600

    added file to help debug simulation scripts for all networks
    Former-commit-id: 1bb71686e519025a2b7a32f44e5c852495b97f76 [formerly c583fe23587a3e1051a8e80cdf73a6f26b952583] [formerly 497f190a5992228a8fe8d0aba8494ff3360e8304 [formerly 02b7a6339719ee034a04fe57745776da813e0870]]
    Former-commit-id: f95b1f98e4d4a567c99617c6c7b1676fc564e6a2
    Former-commit-id: 4a3008a00f680b7ec3b45fcfaf6f2d9100d8d074 [formerly f3f1c0210838880db012d79bec2caf9ee02bad97]
    Former-commit-id: b7ae7e9ac20883c5776b520ebe166051f040c7ec

---
+++ simulation-study/simulation-scripts/debug.jl
@@ -0,0 +1,28 @@
+# TEST RUNS
+using InPhyNet, PhyloNetworks
+include("helpers/helpers.jl")
+include("helpers/simple-plot-fxns.jl")
+
+# All done w/ replicate 1, maxsubsetsize 15, dmethod internode_count
+# Format: netid (proportion drops from 1, proportion hits 0):
+# - n50r2       (0.43, 0.92)
+# - n50r5       (0.57, 1.01)
+# - n100r5      (0.43, 0.79)
+# - n100r10     (ERROR)
+netid = "n100r10"
+replicatenum = 1
+maxsubsetsize = 15
+dmethod = "internode_count"
+nsim = 500
+
+truenet, constraints, D, namelist = loadPerfectData(netid, replicatenum, maxsubsetsize, dmethod)
+esterrors, gausserrors, constraintdiffs, nretics_est =
+    monophyleticRobustness(truenet, constraints, D, namelist, nsim=nsim, displayprogress=true)
+constraintdiffs = sum(constraintdiffs, dims=1)[1,:]
+
+plot_negative_one_prop(netid, upperTriangStd(D))
+
+# DEBUG
+using Revise, NetMerge, PhyloNetworks
+include("helpers/helpers.jl")
+
