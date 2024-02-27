c0c442519d9686fddae4ba5c2a0bee48449bbe25
NathanKolbow <nathankolbow@gmail.com>
2024-02-27 13:58:39 -0600

    ACTUALLY fixed algo so it can handle when one node leads to 2 hybrids
    Former-commit-id: 5273e408d8d3e8cb98faa565849adf32c259e078 [formerly 9d963c0ae30d0167983256cd1d6c61e0db703a87]
    Former-commit-id: 59aa3efb4cd1db4dabde277e744adb5d4d952f5f
    Former-commit-id: 9eede27622c9b10c3477484613445d73ad51bb5e

--- simulation-study/simulation-scripts/debug.jl
+++ simulation-study/simulation-scripts/debug.jl
@@ -5,11 +5,11 @@ include("helpers/simple-plot-fxns.jl")
 
 # All done w/ replicate 1, maxsubsetsize 15, dmethod internode_count
 # Format: netid (proportion drops from 1, proportion hits 0):
-# - n50r2       (0.43, 0.92)
-# - n50r5       (0.57, 1.01)
-# - n100r5      (0.43, 0.79)
+# - n50r2       (0.48, 0.92) - prop-1: 0.59
+# - n50r5       (0.52, 1.07) - prop-1: 0.49
+# - n100r5      (0.49, 0.91) - prop-1: 0.58
 # - n100r10     (ERROR)
-netid = "n50r2"
+netid = "n100r10"
 replicatenum = 1
 maxsubsetsize = 15
 dmethod = "internode_count"
@@ -23,6 +23,6 @@ constraintdiffs = sum(constraintdiffs, dims=1)[1,:]
 plot_negative_one_prop(netid, upperTriangStd(D))
 
 # DEBUG
-using Revise, NetMerge, PhyloNetworks
+using Revise, InPhyNet, PhyloNetworks
 include("helpers/helpers.jl")
 
