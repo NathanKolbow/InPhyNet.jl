23b9b73eae99eae0a2e5aefb359bb8e13c5a84d8
NathanKolbow <nathankolbow@gmail.com>
2024-02-29 13:32:32 -0600

    tested simulations to make sure they run well & implemented internode distance rather than count
    Former-commit-id: 5ee76585640f8682f7428d6db1a0b75c01e4ac8c [formerly bfc898741c3bbce1e0f576cdd58387c2ded595c8]
    Former-commit-id: fda829efaf97f773bf946609387cb24c4f123e98 [formerly 56fe997d4001c79c3f8d5f9e8819b36e91a83a29]
    Former-commit-id: 2f64ddfb9c1b7dda6fbacf02c8eba598a9271d3b
    Former-commit-id: f5d83020c94f36698c67351accadb17a6029ce9d
    Former-commit-id: 12171d5d1e75773232d2fcff37c9472e61ffaef4

--- src/inphynet/subsetdecomp/njhierarchdecomp.jl
+++ src/inphynet/subsetdecomp/njhierarchdecomp.jl
@@ -64,29 +64,40 @@ function pruneTruthFromDecomp(truenet::HybridNetwork, subsets::AbstractVector{<:
         # Sometimes, the given leafset has incoming reticulations that originate
         # from outside of its subnetwork. In this case, the reticulations are kept
         # in the pruned network but given the root as their origin, which is not desired.
-        true_dict = Dict(n.number => n for n in truenet.node)
-        for temp_node in tempnet.node
-            true_node = true_dict[temp_node.number]
-            temp_numbers = [n.number for n in getchildren(temp_node)]
-            true_numbers = [n.number for n in getchildren(true_node)]
+        looping = true
+        while looping
+            looping = false
+            true_dict = Dict(n.number => n for n in truenet.node)
+            for temp_node in tempnet.node
+                true_node = true_dict[temp_node.number]
+                temp_numbers = [n.number for n in getchildren(temp_node)]
+                true_numbers = [n.number for n in getchildren(true_node)]
 
-            length(temp_numbers) == length(true_numbers) || continue
+                length(temp_numbers) == length(true_numbers) || continue
 
-            if length(temp_numbers) == 2
-                if !((temp_numbers[1] == true_numbers[1] && temp_numbers[2] == true_numbers[2]) ||
-                    (temp_numbers[1] == true_numbers[2] && temp_numbers[2] == true_numbers[1]))
-                    # Remove the hybrid edges attached here
-                    @warn "`pruneTruthFromDecomp` HAS NOT BEEN SUFFICIENTLY TESTED TO TRUST IN ITS OUTPUT YET. VERIFY SOME OF THE RESULTS RECEIVED BEFORE CONTINUING."
-                    for temp_edge in temp_node.edge
-                        if temp_edge.hybrid PhyloNetworks.deletehybridedge!(tempnet, temp_edge) end
+                if length(temp_numbers) == 2
+                    if !((temp_numbers[1] == true_numbers[1] && temp_numbers[2] == true_numbers[2]) ||
+                        (temp_numbers[1] == true_numbers[2] && temp_numbers[2] == true_numbers[1]))
+                        # Remove the hybrid edges attached here
+                        @warn "`pruneTruthFromDecomp` HAS NOT BEEN SUFFICIENTLY TESTED TO TRUST IN ITS OUTPUT YET. VERIFY SOME OF THE RESULTS RECEIVED BEFORE CONTINUING."
+                        for temp_edge in temp_node.edge
+                            if temp_edge.hybrid
+                                PhyloNetworks.deletehybridedge!(tempnet, temp_edge)
+                                looping = true
+                            end
+                        end
                     end
-                end
-            elseif length(temp_numbers) == 1
-                if temp_numbers[1] != true_numbers[1]
-                    # Remove the hybrid edges attached here
-                    @warn "`pruneTruthFromDecomp` HAS NOT BEEN SUFFICIENTLY TESTED TO TRUST IN ITS OUTPUT YET. VERIFY SOME OF THE RESULTS RECEIVED BEFORE CONTINUING."
-                    for temp_edge in temp_node.edge
-                        if temp_edge.hybrid PhyloNetworks.deletehybridedge!(tempnet, temp_edge) end
+                elseif length(temp_numbers) == 1
+                    if temp_numbers[1] != true_numbers[1]
+                        # Remove the hybrid edges attached here
+                        @warn "`pruneTruthFromDecomp` HAS NOT BEEN SUFFICIENTLY TESTED TO TRUST IN ITS OUTPUT YET. VERIFY SOME OF THE RESULTS RECEIVED BEFORE CONTINUING."
+                        looping = true
+                        for temp_edge in temp_node.edge
+                            if temp_edge.hybrid
+                                PhyloNetworks.deletehybridedge!(tempnet, temp_edge)
+                                looping = true
+                            end
+                        end
                     end
                 end
             end
