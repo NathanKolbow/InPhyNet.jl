e0a45d3d22886cc2602d326e5c94378eb6fe301f
NathanKolbow <nathankolbow@gmail.com>
2024-03-07 15:47:31 -0600

    added plots
    Former-commit-id: f662df15311610a9347353fb79f5737cd729f128
    Former-commit-id: c69f1d2959516b6bf4ed679811b5c4aee714928e

--- simulation-study/results/figs.R
+++ simulation-study/results/figs.R
@@ -3,10 +3,15 @@ library(ggplot2)
 
 options(dplyr.summarise.inform = FALSE)
 
-full_df <- rbind(
-    read.csv("simulation-study/data/output/n50r2.csv"),
-    read.csv("simulation-study/data/output/n50r5.csv")
-) %>%
+result_files <- dir("simulation-study/data/output/")
+result_files <- result_files[grepl("n[0-9]*r[0-9]*.csv", result_files)]
+
+full_df <- lapply(
+    result_files,
+    function(x) {
+        read.csv(paste0("simulation-study/data/output/", x))
+    }) %>%
+    do.call("rbind", .) %>%
     mutate(
         netid = as.factor(paste0("n", numtaxa, "r", nretics_true)),
         constraint_sizes = NULL # not using these for now
@@ -42,10 +47,13 @@ plot_neg_one_props <- function(df, step_size = 0.1, std0=1, title = "") {
 (p <- plot_neg_one_props(full_df, step_size = 0.025))
 
 
-# Accuracy vs. everything
-gg_df <- filter(full_df, estRFerror >= 0)
+# Accuracy vs. everything (n50r2 & n50r5)
+gg_df <- filter(full_df, estRFerror >= 0 & (netid %in% c("n50r2", "n50r5"))) %>%
+    sample_n(1e4)
+
+
 ggplot(gg_df, aes(x = gauss_error, y = constraint_error_sum, color = estRFerror)) +
-    geom_point() +
+    geom_jitter(width = 0, height = 1, alpha = 0.5) +
     facet_grid(netid ~ max_subset_size) +
     scale_color_gradientn(
         colors = rainbow(7),
@@ -57,7 +65,41 @@ ggplot(gg_df, aes(x = gauss_error, y = constraint_error_sum, color = estRFerror)
 gg_df %>%
     filter(gauss_error <= 2) %>%
     ggplot(aes(x = gauss_error, y = constraint_error_sum, color = estRFerror)) +
-        geom_point() +
+        geom_jitter(width = 0, height = 1, alpha = 0.5) +
+        facet_grid(netid ~ max_subset_size) +
+        scale_color_gradientn(colors = rainbow(7)) +
+    labs(x = "Noise = Normal(x, x)", y = "Sum of constraint errors (HWCD)") +
+    ggtitle("Successful runs only")
+
+# Accuracy vs. everything (n200r10 & n200r20)
+gg_df <- filter(full_df, estRFerror >= 0 & (netid %in% c("n200r10", "n200r20")))
+
+
+ggplot(gg_df, aes(x = gauss_error, y = constraint_error_sum, color = estRFerror)) +
+    geom_jitter(width = 0, height = 1, alpha = 0.5) +
+    facet_grid(netid ~ max_subset_size) +
+    scale_color_gradientn(colors = rainbow(7)) +
+    labs(x = "Noise = Normal(x, x)", y = "Sum of constraint errors (HWCD)") +
+    ggtitle("Successful runs only")
+ggplot(gg_df, aes(x = gauss_error, y = constraint_error_sum, color = majortreeRF)) +
+    geom_jitter(width = 0, height = 1, alpha = 0.5) +
+    facet_grid(netid ~ max_subset_size) +
+    scale_color_gradientn(colors = rainbow(7)) +
+    labs(x = "Noise = Normal(x, x)", y = "Sum of constraint errors (HWCD)") +
+    ggtitle("Successful runs only")
+
+gg_df %>%
+    filter(gauss_error <= 2) %>%
+    ggplot(aes(x = gauss_error, y = constraint_error_sum, color = estRFerror)) +
+        geom_jitter(width = 0, height = 1, alpha = 0.5) +
+        facet_grid(netid ~ max_subset_size) +
+        scale_color_gradientn(colors = rainbow(7)) +
+    labs(x = "Noise = Normal(x, x)", y = "Sum of constraint errors (HWCD)") +
+    ggtitle("Successful runs only")
+gg_df %>%
+    filter(gauss_error <= 2) %>%
+    ggplot(aes(x = gauss_error, y = constraint_error_sum, color = majortreeRF)) +
+        geom_jitter(width = 0, height = 1, alpha = 0.5) +
         facet_grid(netid ~ max_subset_size) +
         scale_color_gradientn(colors = rainbow(7)) +
     labs(x = "Noise = Normal(x, x)", y = "Sum of constraint errors (HWCD)") +
