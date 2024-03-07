f813ad7ea0151269a3ce568c0df8f20526ec035b
NathanKolbow <nathankolbow@gmail.com>
2024-03-07 10:43:51 -0600

    stopped tracking results files
    Former-commit-id: b4521fe23f144149f0e167881c6c4e238fb1e168 [formerly 28dfd7c8ae14d8fe31abb9f03e0514e84bd8b3d9]
    Former-commit-id: b2798247bb167df60e242b9148cc966346fd04ed [formerly 1f3b013d021d164be71e5b6c180dadb07879f96f]
    Former-commit-id: 2e1b59460442bda8e5e787ec1ada5778ef9d26f3
    Former-commit-id: 5a061e4f2ab72ef041da1ba435f025ad838b935a
    Former-commit-id: cbf0f9a164512f1b9cc45ce13d62bccb52b5b0a6

--- simulation-study/study.sh
+++ simulation-study/study.sh
@@ -5,7 +5,7 @@ for top in n50r2 n50r5 # n100r5 n100r10 ...
 do
     for maxsubsetsize in 5 10 15 20 25
     do
-        for rep in $(seq 1 100)
+        for rep in $(seq 1 4)   # going to 4 right now just to get some preliminary results, we don't need every replicate yet...
         do
             echo "julia --project=../.. -t6 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log"
             julia --project=../.. -t6 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 10000 2>> perfect_subsets.log
