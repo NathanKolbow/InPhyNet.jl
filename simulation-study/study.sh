0595a2ec455d9717e26f94c4714f7a5a4191e63e
NathanKolbow <nathankolbow@gmail.com>
2024-03-07 13:55:20 -0600

    small adjustment
    Former-commit-id: 66482744252d79d120a7807fcdaf4c981c337146 [formerly c08af76178423871f52b76e479b17994d2dcdfa7] [formerly 8380d40d88106cffc72ed420c197f0580af4bdc9 [formerly ff1497be2d265d1dd27b862e099d80c4a77996e8]]
    Former-commit-id: 124a0fd1f190e1a9b2000a20671366c765fe137b
    Former-commit-id: 91595b2f2616679a6df52002bc9d39f7788d55dc [formerly c8dc64dd616311c16bdd84e672d137ee01f8045b]
    Former-commit-id: af4fd9f85301b33dc5a25f59cf4bfa5cbaee4ab4

--- simulation-study/study.sh
+++ simulation-study/study.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Run from simulation-study/simulation-scripts/
-for rep in $(seq 1 100)   # going to 4 right now just to get some preliminary results, we don't need every replicate yet...
+for rep in $(seq 1 10)   # going to 4 right now just to get some preliminary results, we don't need every replicate yet...
 do
     for maxsubsetsize in 5 10 15 20 25
     do
-        for top in n50r2 n50r5 # n100r5 n100r10 ...
+        for top in n50r2 n50r5 n100r5 n100r10 n200r10 n200r20 n500r25 n500r50 n1000r50 n1000r100
         do
-            echo "julia --project=../.. -t6 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 10000 2>> perfect_subsets.log"
-            julia --project=../.. -t6 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log
+            echo "julia --project=../.. -t4 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log"
+            julia --project=../.. -t4 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log
         done
     done
 done
