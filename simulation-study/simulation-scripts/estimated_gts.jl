bb41f7a02e9ea45d19e758a14d764c9c0a547157
NathanKolbow <nathankolbow@gmail.com>
2024-04-15 16:15:54 -0500

    added proper results saving, S value, constraint estimation runtimes to sim scripts

--- simulation-study/simulation-scripts/estimated_gts.jl
+++ simulation-study/simulation-scripts/estimated_gts.jl
@@ -1,8 +1,6 @@
 # Full pipeline going from truenet > simulated gene trees > simulated sequences > inferred gene trees > inferred networks > netmerge
 
-error("need to track the values we talk about in meeting w/ Kevin and Claudia and implement `save_estimated_gts_results`.")
-
-if length(ARGS) != 6
+if length(ARGS) != 5
     error("Usage: julia --project=X -tY estimated_gts.jl \"<true network abbreviation>\" <replicate number> <number of loci> <sequence length> <ils level (low/med/high)>")
 end
 
@@ -16,7 +14,7 @@ ils_level = ARGS[5]
 (ngt == 100 || ngt == 1000) || error("Number of loci $ngt not allowed; must be 100 or 1,000.")
 (seq_len == 100 || seq_len == 1000) || error("Sequence length $seq_len not allowed; must be 100 or 1,000.")
 maxsubsetsize = 15
-dmethod = "internode_count"
+dmethod = "AGIC"
 ###########################
 nhybrids = parse(Int64, split(netid, "r")[2])
 data_dir = "/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/simulation-scripts/data"
@@ -25,7 +23,7 @@ data_dir = "/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/si
 include("helpers/helpers.jl")
 
 # 1. gather ground truth network, constraint, distance matrix, and namelist
-println("Loading data")
+@info "Loading data"
 truenet = load_true_net_ils_adjusted(netid, replicatenum, ils_level)
 seed = parse(Int64, "$(truenet.numTaxa)42$(truenet.numHybrids)42$(replicatenum)")
 for e in truenet.edge
@@ -33,7 +31,7 @@ for e in truenet.edge
 end
 
 # 2. simulate gene trees
-println("Simulating gene trees")
+@info "Simulating gene trees"
 using PhyloCoalSimulations
 truegt_file = joinpath(data_dir, "truegt_$(netid)_$(replicatenum)_$(ngt).treefile")
 gts = nothing
@@ -47,39 +45,39 @@ else
 end
 
 # 3. simulate sequences w/ seq-gen
-println("Simulating sequences")
+@info "Simulating sequences"
 seq_file_prefix = joinpath(data_dir, "seqfile_$(netid)_$(replicatenum)_$(ngt).phy")
 Random.seed!(seed)
 seq_files = simulate_sequence_data(gts, truegt_file, seq_file_prefix)
 
 # 4. infer gene trees w/ iqtree
-println("Inferring gene trees")
+@info "Inferring gene trees"
 estgt_file = joinpath(data_dir, "estgt_$(netid)_$(replicatenum)_$(ngt).treefile")
 Random.seed!(seed)
 estimate_gene_trees(seq_files, estgt_file)
 
 # 5. estimate an NJ tree for subset decomposition
-println("Estimating NJ tree")
+@info "Estimating NJ tree"
 est_gts = readMultiTopology(estgt_file)
 est_D, est_namelist = calculateAGIC(est_gts)
 nj_df = DataFrame(est_D, est_namelist)
 nj_tre = nj(nj_df)
 
 # 6. decompose into disjoint subsets
-println("Decomposing taxa into disjoint subsets")
+@info "Decomposing taxa into disjoint subsets"
 subsets = sateIdecomp(nj_tre, maxsubsetsize)
 if minimum([length(s) for s in subsets]) < 4
     error("Smallest subset must have at least 4 taxa for SNaQ.")
 end
 
 # 7. infer constraints w/ SNaQ
-println("Inferring networks")
+@info "Inferring networks"
 net_file = joinpath(data_dir, "estnets_$(netid)_$(replicatenum)_$(maxsubsetsize)_$(dmethod)_$(ngt).netfile")
 Random.seed!(seed)
-est_constraints = infer_constraints(estgt_file, net_file, subsets, nhybrids)
+est_constraints, est_constraint_runtimes = infer_constraints(estgt_file, net_file, subsets, nhybrids)
 
 # 8. InPhyNet inference
-println("Merging networks")
+@info "Merging networks"
 mnet = nothing
 try
     mnet = netnj(est_D, est_constraints, est_namelist)
@@ -87,4 +85,7 @@ catch e
 end
 
 # 9. Save results
-# save_estimated_gts_results
+@info "Saving results"
+save_estimated_gts_results(netid, truenet, replicatenum, ngt,
+    ils_level, maxsubsetsize, dmethod, seq_len,
+    mnet, est_constraints, est_gts, est_constraint_runtimes)
