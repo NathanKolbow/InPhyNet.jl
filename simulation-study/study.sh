b4c910796bd600c302b3f0b58eea8b0baa08f33b
NathanKolbow <nathankolbow@gmail.com>
2024-04-15 16:15:54 -0500

    added proper results saving, S value, constraint estimation runtimes to sim scripts
    Former-commit-id: bb41f7a02e9ea45d19e758a14d764c9c0a547157
    Former-commit-id: 6104e3500b8a4f05e8f103f3017b2bd6fed16af4

--- simulation-study/study.sh
+++ simulation-study/study.sh
@@ -18,7 +18,25 @@ for rep in $(seq 1 10)   # going to 10 right now just to get some preliminary re
 do
     for maxsubsetsize in 5 10 15 20 25 30
     do
-        echo "julia --project=../.. -t4 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log"
-        julia --project=../.. -t4 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log
+        echo "julia --project=../.. -t8 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log"
+        julia --project=../.. -t8 ./perfect_subsets.jl ${top} ${rep} ${maxsubsetsize} "internode_count" 1000 2>> perfect_subsets.log
+    done
+done
+
+# Small subset for testing out estimated_gts.jl
+set -e
+top=n50r2
+for rep in 1
+do
+    for nloci in 100 1000
+    do
+        for seq_len in 100 1000
+        do
+            for ils_level in low high
+            do
+                echo "JULIA_DEBUG=Main julia --project=../.. -t8 ./estimated_gts.jl ${top} ${rep} ${nloci} ${seq_len} ${ils_level}"
+                JULIA_DEBUG=Main julia --project=../.. -t8 ./estimated_gts.jl ${top} ${rep} ${nloci} ${seq_len} ${ils_level}
+            done
+        done
     done
 done
