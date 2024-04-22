fbafba9d5fa0c3657a78ad5c2566e78ea283840d
NathanKolbow <nathankolbow@gmail.com>
2024-04-22 12:36:50 -0500

    made results writing thread-safe and added a -2 return for constraint errors
    Former-commit-id: d41e7944c4b8c76383f70156be356ad08008ccfc [formerly 8d3685d44fc4d42bbb879f6fb667bf6dd2236154] [formerly 13cd0765dfc925c56f82174222bdbce683078c49] [formerly 8b4824c67b8a87ae7ad1505dd6eeb72d4ee98e2a [formerly 0e8507e8d9056b3ccaf5fe1e6fbb0a77cbc74cab]] [formerly cca218690789a7c73dc0d23f7bf6aac31fe88aeb [formerly 9271a2bf2c70693b45468405c86a3391ceb74211]]
    Former-commit-id: aef3233a50137b2658f5a70d64ac86d247c9e705
    Former-commit-id: 2e7d052f6316b8de4ce9a9221fbf8f27ceb07680 [formerly a9fe87ff90422ef115aa4f49913501923861a351]
    Former-commit-id: 19fd0b078ce7d8f7ae9883744476d115923a6659 [formerly 102a2df90aab296e14ba7236753d15270e23f786]
    Former-commit-id: 3f99aa59d76dfa2d3894c987527db72487b10205
    Former-commit-id: e2a49f6c72a6449ec3d57e7eb1be850cd6130234

--- simulation-study/simulation-scripts/helpers/save-results.jl
+++ simulation-study/simulation-scripts/helpers/save-results.jl
@@ -1,7 +1,7 @@
 # Need to map out how the results will be stored first.
 include("misc-metrics.jl")
 
-using CSV, DataFrames
+using CSV, DataFrames, Pidfile
 
 const AV = AbstractVector
 function savePerfectResults(truenet::HybridNetwork, constraints::AV{HybridNetwork}, esterrors::AV{<:Real}, majortreeRFs::AV{<:Real},
@@ -26,22 +26,27 @@ function savePerfectResults(truenet::HybridNetwork, constraints::AV{HybridNetwor
     constraint_sizes = replace(constraint_sizes[2:(length(constraint_sizes)-1)], " " => "")
 
     # Put together and save data
-    df = DataFrame(
-        numtaxa=repeat([truenet.numTaxa - 1], nrows),
-        nretics_true=repeat([truenet.numHybrids], nrows),
-        nretics_est=nretics_est,
-        nretics_inside=repeat([nretics_inside], nrows),
-        nretics_outside=repeat([nretics_outside], nrows),
-        nretics_duplicated=repeat([nretics_duplicated], nrows),
-        constraint_sizes=constraint_sizes,
-        max_subset_size=max_subset_size,
-        estRFerror=esterrors,
-        majortreeRF=majortreeRFs,
-        gauss_error=gausserrors,
-        constraint_error_sum=constraintdiffs,
-        replicate_num=repeat([replicate_num], nrows)
-    )
-    CSV.write(output_path, df, append=true)
+    lk = mkpidlock(output_path*".lk")
+    try
+        df = DataFrame(
+            numtaxa=repeat([truenet.numTaxa - 1], nrows),
+            nretics_true=repeat([truenet.numHybrids], nrows),
+            nretics_est=nretics_est,
+            nretics_inside=repeat([nretics_inside], nrows),
+            nretics_outside=repeat([nretics_outside], nrows),
+            nretics_duplicated=repeat([nretics_duplicated], nrows),
+            constraint_sizes=constraint_sizes,
+            max_subset_size=max_subset_size,
+            estRFerror=esterrors,
+            majortreeRF=majortreeRFs,
+            gauss_error=gausserrors,
+            constraint_error_sum=constraintdiffs,
+            replicate_num=repeat([replicate_num], nrows)
+        )
+        CSV.write(output_path, df, append=true)
+    finally
+        close(lk)
+    end
 end
 
 
@@ -93,38 +98,43 @@ function save_estimated_gts_results(netid::String, true_network::HybridNetwork, 
 
     # Create a DF then write the results
     @debug "Creating DF"
-    df = DataFrame(
-        # input param data
-        net_id = [netid],
-        replicate_num = [replicatenum],
-        n_loci = [nloci],
-        seq_len = [seq_len],
-        ils_level = [ils_level],
-        max_subset_size = [max_subset_size],
-        distance_method = [distance_method],
-
-        # basic topological info
-        num_taxa = [num_taxa],
-        num_retics_true = [num_retics_true],
-        num_retics_est = [num_retics_est],
-        num_retics_inside = [nretics_inside],
-        num_retics_outside = [nretics_outside],
-        num_retics_duplicated = [nretics_duplicated],
-
-        # inferred newick
-        est_newick = [est_newick],
-
-        # inference errors
-        est_net_hwcd = [est_net_hwcd],
-        est_net_hwcd_only_identified_retics = ["not implemented"],
-        est_constraint_hwcd_sum = [est_constraint_hwcd_sum],
-
-        # time taken
-        time_seconds_parallel = [maximum(est_constraint_runtimes) + inphynet_runtime],
-        time_seconds_series = [sum(est_constraint_runtimes) + inphynet_runtime]
-    )
-    @debug "Writing to file $(output_path)"
-    CSV.write(output_path, df, append=true)
+    lk = mkpidlock(output_path*".lk")
+    try
+        df = DataFrame(
+            # input param data
+            net_id = [netid],
+            replicate_num = [replicatenum],
+            n_loci = [nloci],
+            seq_len = [seq_len],
+            ils_level = [ils_level],
+            max_subset_size = [max_subset_size],
+            distance_method = [distance_method],
+    
+            # basic topological info
+            num_taxa = [num_taxa],
+            num_retics_true = [num_retics_true],
+            num_retics_est = [num_retics_est],
+            num_retics_inside = [nretics_inside],
+            num_retics_outside = [nretics_outside],
+            num_retics_duplicated = [nretics_duplicated],
+    
+            # inferred newick
+            est_newick = [est_newick],
+    
+            # inference errors
+            est_net_hwcd = [est_net_hwcd],
+            est_net_hwcd_only_identified_retics = ["not implemented"],
+            est_constraint_hwcd_sum = [est_constraint_hwcd_sum],
+    
+            # time taken
+            time_seconds_parallel = [maximum(est_constraint_runtimes) + inphynet_runtime],
+            time_seconds_series = [sum(est_constraint_runtimes) + inphynet_runtime]
+        )
+        @debug "Writing to file $(output_path)"
+        CSV.write(output_path, df, append=true)
+    finally
+        close(lk)
+    end
 end
 
 
@@ -140,7 +150,7 @@ function estimated_sims_already_performed(netid::String, replicatenum::Int64, ng
 end
 
 
-function perfect_sims_already_performed(netid::String, replicatenum::Int64, maxsubsetsize::Int64)
+function n_perfect_sims_already_performed(netid::String, replicatenum::Int64, maxsubsetsize::Int64)
     output_path = get_output_filepath(netid)
     if !isfile(output_path) return false end
     ntaxa = split(netid, "r")
@@ -148,5 +158,5 @@ function perfect_sims_already_performed(netid::String, replicatenum::Int64, maxs
     ntaxa = parse(Int64, split(ntaxa[1], "n")[2])
 
     df = CSV.read(output_path, DataFrame)
-    return nrow(filter(row -> row.max_subset_size == maxsubsetsize, df)) > 0
+    return nrow(filter(row -> row.replicate_num == replicatenum && row.max_subset_size == maxsubsetsize, df))
 end
