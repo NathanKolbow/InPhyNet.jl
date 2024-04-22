93406cba97cca974ca81073e52aa51a35803e807
NathanKolbow <nathankolbow@gmail.com>
2024-04-22 14:20:37 -0500

    added metric to perfect sims for HWCD(true, est) w/ unidentified retics removed
    Former-commit-id: 6096c2aa3d40293aa7efd49e1ded08ce099e7153 [formerly 769a6e08bc0dbc65a6acff11abf9e38b183e98ed]
    Former-commit-id: 4ca44f0b5685abf33e5295bd7b1e1c00d64de6b6 [formerly 102baf7a93372653694955b6171dfba0f82da17e]
    Former-commit-id: 86678c8f4e6a1ba74b232277e01c62c8f571362e
    Former-commit-id: 1b002b775a4071043ebcadad4e761671fd60424b
    Former-commit-id: ebd8e28a32be904a7678687806b3618635d56e7b

--- simulation-study/simulation-scripts/helpers/misc-metrics.jl
+++ simulation-study/simulation-scripts/helpers/misc-metrics.jl
@@ -39,3 +39,32 @@ function calculate_net_logpseudolik(net::HybridNetwork, df::PhyloNetworks.DataCF
 
     return PhyloNetworks.logPseudoLik(df)
 end
+
+
+"""
+Gets the HWCD error between `true_net` and `mnet` after removing reticulations in
+`true_net` that do not appear in `mnet`. Hybrids must match in name for this
+function to work properly.
+"""
+function get_error_without_missing_retics(true_net::HybridNetwork, mnet::HybridNetwork; try_root_at_node::String="OUTGROUP")
+    true_net_copy = readTopology(writeTopology(true_net))
+    true_hybs = true_net_copy.hybrid
+    mnet_hybs = mnet.hybrid
+    retics_to_remove = setdiff([h.name for h in true_hybs], [h.name for h in mnet_hybs])
+
+    for retic_name in retics_to_remove
+        hybnode = true_hybs[findfirst([h.name for h in true_hybs] .== retic_name)]
+        PhyloNetworks.deletehybridedge!(true_net_copy, getparentedgeminor(hybnode))
+    end
+
+    try
+        rootatnode!(true_net, "OUTGROUP")
+    catch
+    end
+    try
+        rootatnode!(mnet, "OUTGROUP")
+    catch
+    end
+    
+    return hardwiredClusterDistance(true_net_copy, mnet, true)
+end
