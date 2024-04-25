46c397c59a4379caabb32b5dbd8c0e9616479e3d
NathanKolbow <nathankolbow@gmail.com>
2024-04-25 15:14:08 -0500

    made script file for later experiment
    Former-commit-id: 5ae93e4f756aa6048a00e7189a3600afe9393c0f [formerly 6ae13fa91e2971d9172eac7d6854dfb376e9b14c]
    Former-commit-id: 67f17b116692044b8681157228448fe969ce0651
    Former-commit-id: dc5ac2218f4f8a5a5a0f3d86a21acb3b1cc251c0

---
+++ simulation-study/simulation-scripts/temp_studies/major_tree_only_siblings.jl
@@ -0,0 +1,6 @@
+######################################## README ########################################
+# In this file, we experiment to see which of the following gives better results:
+# - taxa can be considered siblings if they are siblings in any displayed tree
+# - taxa are only siblings if they are siblings in the major tree
+########################################################################################
+
