a9511ef9d65b9c6db8bc53425e726b8166083b7c
NathanKolbow <nathankolbow@gmail.com>
2024-04-25 14:40:10 -0500

    more figures and experiment data & scripts
    Former-commit-id: 5b931b87d22a0f8a3013117fb01d9f0370b4dfd8
    Former-commit-id: 949fb46eb94943f9f508f31d3f8259b6bc9f88f2

--- simulation-study/simulation-scripts/temp_studies/all_constraints_have_outgroup.R
+++ simulation-study/simulation-scripts/temp_studies/all_constraints_have_outgroup.R
@@ -4,21 +4,37 @@ options(dplyr.summarise.inform = FALSE)
 theme_set(theme_linedraw())
 
 df <- read.csv("/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/simulation-scripts/temp_studies/all_constraints_have_outgroups.csv") %>%
-    mutate(max_subset_size_label = paste0("m = ", max_subset_size),
+    mutate(max_subset_size_label = paste0("max subset size = ", max_subset_size),
+        gauss_error_level_og = ordered(gauss_error_level, levels = c("low", "med", "high")),
         gauss_error_level = ordered(paste0("gauss = ", gauss_error_level), levels = c("gauss = low", "gauss = med", "gauss = high")))
 
 # Proportion of successful runs
-gg_df <- df %>%
-    group_by(netid, replicate_num, max_subset_size_label, gauss_error_level) %>%
-    summarise(prop_runs_good_default = mean(net_hwcd_default >= 0),
-        prop_runs_good_outgroup = mean(net_hwcd_outgroup >= 0))
+# gg_df <- df %>%
+#     group_by(netid, replicate_num, max_subset_size_label, gauss_error_level) %>%
+#     summarise(prop_runs_good_default = mean(net_hwcd_default >= 0),
+#         prop_runs_good_outgroup = mean(net_hwcd_outgroup >= 0))
+# ggplot(gg_df, aes(x = replicate_num, y = prop_runs_good_default, shape = "Default", color = "Default")) +
+#     geom_point(size = 3) +
+#     geom_point(aes(x = replicate_num, y = prop_runs_good_outgroup, shape = "Outgroup", color = "Outgroup"), size = 3) +
+#     facet_grid(gauss_error_level ~ netid + max_subset_size_label) +
+#     labs(x = "Replicate", y = "Proportion of runs succeeded") +
+#     scale_y_continuous(limits = c(0, 1))
 
-ggplot(gg_df, aes(x = replicate_num, y = prop_runs_good_default, shape = "Default", color = "Default")) +
+# Proportion of successful runs w/ groupings
+gg_df <- df %>%
+    group_by(netid, replicate_num, max_subset_size_label, gauss_error_level, gauss_error_level_og) %>%
+    summarise(prop_runs_good_default = mean(net_hwcd_default >= 0),
+        prop_runs_good_outgroup = mean(net_hwcd_outgroup >= 0)) %>%
+    mutate(prop_better = prop_runs_good_outgroup - prop_runs_good_default,
+        result = ifelse(prop_better < 0, "Default", ifelse(prop_better == 0, "Tie", "All have outgroup")))
+ggplot(gg_df, aes(x = gauss_error_level_og, y = prop_better, color = result, shape = result)) +
     geom_point(size = 3) +
-    geom_point(aes(x = replicate_num, y = prop_runs_good_outgroup, shape = "Outgroup", color = "Outgroup"), size = 3) +
-    facet_grid(gauss_error_level ~ netid + max_subset_size_label) +
-    labs(x = "Replicate", y = "Proportion of runs succeeded") +
-    scale_y_continuous(limits = c(0, 1))
+    facet_grid(max_subset_size_label ~ netid) +
+    geom_abline(slope = 0, intercept = 0, linetype = "dashed") +
+    labs(x = "Gaussian Noise Level",
+        y = "(Proportion default runs succeeded) - (Proportion outgroup runs succeeded)",
+        color = "Which succeeds more often?") +
+    scale_shape_discrete(guide = "none")
 
 # HWCD
 gg_df <- filter(df, net_hwcd_default >= 0 & net_hwcd_outgroup >= 0)
@@ -27,15 +43,17 @@ ggplot(gg_df) +
     geom_boxplot(aes(group = constraint_diffs_outgroup, x = constraint_diffs_outgroup, y = net_hwcd_outgroup, color = "Outgroup", shape = "Outgroup"), alpha = 0.75) +
     geom_abline(slope = 1, intercept = 0)
 
+# HWCD differences
 gg_df <- filter(df, net_hwcd_default >= 0 & net_hwcd_outgroup >= 0) %>%
-    group_by(netid, replicate_num, max_subset_size_label, gauss_error_level) %>%
+    group_by(netid, replicate_num, max_subset_size_label, gauss_error_level, gauss_error_level_og) %>%
     summarise(mean_hwcd_default = mean(net_hwcd_default),
         mean_hwcd_outgroup = mean(net_hwcd_outgroup)) %>%
     mutate(mean_hwcd_diff = mean_hwcd_default - mean_hwcd_outgroup,
         better_with_outgroups = mean_hwcd_diff > 0)
-ggplot(gg_df, aes(x = gauss_error_level, y = mean_hwcd_diff, color = better_with_outgroups)) +
-    geom_point() +
-    facet_grid(netid ~ max_subset_size_label) +
+ggplot(gg_df, aes(x = gauss_error_level_og, y = mean_hwcd_diff, color = better_with_outgroups)) +
+    geom_point(size = 3) +
+    facet_grid(max_subset_size_label ~ netid) +
     geom_abline(slope = 0, intercept = 0, linetype = "dashed") +
-    annotate("text", x = 1, y = 5, label = ">0: better w/ outgroups", color = "red")
-
+    labs(x = "Gaussian Noise Level",
+        y = "HWCD(default) - HWCD(outgroup)",
+        color = "Better w/ outgroups?")
