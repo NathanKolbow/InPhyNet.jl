40f98b2b82a61bdf38f9f86e69a4a6443e502081
NathanKolbow <nathankolbow@gmail.com>
2024-04-26 14:46:41 -0500

    more figures

--- simulation-study/results/figs.R
+++ simulation-study/results/figs.R
@@ -13,11 +13,6 @@ source("/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/result
 # FIGURES FOR SIMS W/ PERFECT INPUT DATA #
 ##########################################
 
-no_added_error_df <- filter(full_df, constraint_error_sum == 0 & gauss_error == 0 & estRFerror != -1) %>%
-    filter(max_subset_size %in% c(10, 15, 20, 25))  # just so there are fewer things to look @ in the plot pane
-ggplot(no_added_error_df, aes(x = netid, y = estRFerror, group = netid)) +
-    geom_boxplot() +
-    facet_wrap(~max_subset_size)
 
 
 ##################################################
@@ -25,9 +20,9 @@ ggplot(no_added_error_df, aes(x = netid, y = estRFerror, group = netid)) +
 ##################################################
 
 # x = Gauss error, y = NNI error, color = probability to fail
-plot_success_rate_vs_binned_errors(net_df("n50r2"))
-plot_success_rate_vs_binned_errors(net_df("n100r10"))
-plot_success_rate_vs_binned_errors(net_df("n200r10"))
+plot_success_rate_vs_binned_errors("n50r2")
+plot_success_rate_vs_binned_errors("n100r10")
+plot_success_rate_vs_binned_errors("n200r10")
 
 ###################################################################
 # HWCD & HWCD W/O MISSING RETIC FIGURES FOR DATA W/ PERTURBATIONS #
@@ -50,6 +45,7 @@ plot_compare_hwcd_with_and_wo_identified_retics(net_df(df, "n50r2"))
 # HWCD vs binned noise levels boxplots
 plot_hwcd_grouped_boxplots("n50r2", without_extra_retics = TRUE)
 plot_hwcd_grouped_boxplots("n100r5", without_extra_retics = TRUE)
+plot_hwcd_grouped_boxplots("n100r10", without_extra_retics = TRUE)
 plot_hwcd_grouped_boxplots("n200r10", without_extra_retics = TRUE)
 plot_hwcd_grouped_boxplots("n500r25", without_extra_retics = TRUE)
 plot_hwcd_grouped_boxplots("n1000r100", without_extra_retics = TRUE)
@@ -57,6 +53,22 @@ plot_hwcd_grouped_boxplots("n1000r100", without_extra_retics = TRUE)
 # Best replicate plots
 plot_best_replicate_hwcd_grouped_boxplots("n50r2", without_extra_retics = TRUE)
 plot_best_replicate_hwcd_grouped_boxplots("n100r5", without_extra_retics = TRUE)
+plot_best_replicate_hwcd_grouped_boxplots("n100r10", without_extra_retics = TRUE)
 plot_best_replicate_hwcd_grouped_boxplots("n200r10", without_extra_retics = TRUE)
 plot_best_replicate_hwcd_grouped_boxplots("n500r25", without_extra_retics = TRUE)
 plot_best_replicate_hwcd_grouped_boxplots("n1000r100", without_extra_retics = TRUE)
+
+# Best (100*p)% of replicates plot
+plot_best_p_replicates_hwcd_grouped_boxplots("n100r5", p = 0.5, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n100r5", p = 0.25, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n100r5", p = 0.10, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n100r5", p = 0.0001, without_extra_retics = TRUE)
+
+plot_best_p_replicates_hwcd_grouped_boxplots("n200r10", p = 0.5, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n200r10", p = 0.25, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n200r10", p = 0.10, without_extra_retics = TRUE)
+plot_best_p_replicates_hwcd_grouped_boxplots("n200r10", p = 0.0001, without_extra_retics = TRUE)
+
+# Estimated net error vs. input error, binned by Gaussian noise
+plot_est_hwcd_vs_sum_input_error_line("n100r5", without_extra_retics = TRUE)
+plot_est_hwcd_vs_sum_input_error_line("n100r10", without_extra_retics = TRUE)
