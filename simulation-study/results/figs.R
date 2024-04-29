943c09d59e2359969de9ba42542f779611f473c2
NathanKolbow <nathankolbow@gmail.com>
2024-04-29 16:01:11 -0500

    added brute force searching merge pairs and more figures
    Former-commit-id: f97f669bd79e78b43459fc4fa4970321b4a69d6e
    Former-commit-id: 183ed53e335d3e5b33a4757f29e454d9ed0eb3e4

--- simulation-study/results/figs.R
+++ simulation-study/results/figs.R
@@ -14,7 +14,7 @@ source("/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/result
 # FIGURES FOR SIMS W/ PERFECT INPUT DATA #
 ##########################################
 
-plot_no_noise_success_props()
+plot_no_noise_success_props("n50r2")
 
 ##################################################
 # SUCCESS RATE FIGURES FOR DATA W/ PERTURBATIONS #
@@ -28,6 +28,10 @@ plot_success_rate_vs_binned_errors("n200r10")
 plot_success_rate_vs_binned_errors_lineplot("n50r2")
 plot_success_rate_vs_binned_errors_lineplot("n100r5")
 
+# another type of graph
+plot_success_prop_stacked_bar("n50r2")
+plot_success_prop_stacked_bar("n200r10")
+
 ###################################################################
 # HWCD & HWCD W/O MISSING RETIC FIGURES FOR DATA W/ PERTURBATIONS #
 ###################################################################
@@ -36,6 +40,9 @@ plot_success_rate_vs_binned_errors_lineplot("n100r5")
 plot_hwcd_heatmap("n50r2", plot_factor = 2)
 plot_hwcd_heatmap("n50r2", plot_factor = 2, without_extra_retics = TRUE)
 plot_hwcd_heatmap("n50r2", plot_factor = 2, without_extra_retics = TRUE, subset_facet = TRUE)
+plot_hwcd_heatmap("n200r10", plot_factor = 2, without_extra_retics = TRUE, subset_facet = TRUE)
+plot_hwcd_heatmap("n200r10", plot_factor = 2, without_extra_retics = TRUE, subset_facet = FALSE)
+# plot_hwcd_heatmap("n200r10", plot_factor = 2, without_extra_retics = TRUE, mark_minimal = TRUE, subset_facet = TRUE)
 
 # Heatmap w/ std0 as x-axis
 plot_hwcd_heatmap_std0("n50r2", plot_factor = 10, without_extra_retics = TRUE)
