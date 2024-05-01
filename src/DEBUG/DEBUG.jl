8494efff7c5b16ed6f7baf426c5d3e918b6498c6
NathanKolbow <nathankolbow@gmail.com>
2024-05-01 14:55:57 -0500

    bug fix
    Former-commit-id: 078b80fc0fc29ae10acb51f178a1e1eeed6bf512
    Former-commit-id: 1ef66fc1e52bb6917b725942dd78a66ec60b1b58

--- src/DEBUG/DEBUG.jl
+++ src/DEBUG/DEBUG.jl
@@ -16,6 +16,7 @@ while true
             println("SolutionDNEError in $(error_id), removing.")
             remove_error_files(error_id)
         else
+            @info "Found case for netid n$(truenet.numTaxa-1)r$(truenet.numHybrids) ($(typeof(e)), $(error_id))"
             break
         end
     end
@@ -30,12 +31,13 @@ netnj(D, constraints, namelist)
 
 # Narrow down which constraint(s) cause the error
 cs = find_problematic_constraints(D, constraints, namelist)
+cs = constraints[3:4]
 D_reduced, namelist_reduced = reduce_D_namelist(D, cs, namelist)    # sometimes the problem will still occur when D & namelist are reduced, sometimes not
 
 #### DEBUG ####
 
 D_iter, cs_iter, namelist_iter, subnets, reticmap, rootretics, rootreticprocessed = step_inphynet_starter_vars(D, cs, namelist)
-while cs_iter[1].numTaxa > 1
+while size(D_iter, 1) > 1
     D_iter, cs_iter, namelist_iter, subnets, reticmap, rootretics, rootreticprocessed =
         step_inphynet!(D_iter, cs_iter, namelist_iter, subnets, reticmap, rootretics, rootreticprocessed)
 end
@@ -47,6 +49,46 @@ mnet = InPhyNet.placeretics!(mnet, reticmap, copy_retic_names=false)
 InPhyNet.removeplaceholdernames!(mnet)
 
 
+
+
+# MWE
+net = readTopology("(((A)#H1,B),#H1);")
+r = InPhyNet.ReticMap([net])
+
+nodei = net.leaf[1]
+nodej = net.leaf[2]
+
+graph = InPhyNet.Graph(net, includeminoredges=true)
+
+idxnodei = findfirst(net.node .== [nodei])
+idxnodej = findfirst(net.node .== [nodej])
+edgepath = a_star(graph, idxnodei, idxnodej)
+
+nodesinpath = Array{Node}(undef, length(edgepath)+1)
+edgesinpath = Array{Edge}(undef, length(edgepath))
+for (i, gedge) in enumerate(edgepath)
+    srcnode = net.node[gedge.src]
+    dstnode = net.node[gedge.dst]
+
+    if i == 1 nodesinpath[1] = net.node[gedge.src] end
+    nodesinpath[i+1] = dstnode
+
+    netedge = filter(e -> (srcnode in e.node) && (dstnode in e.node), dstnode.edge)[1]
+    edgesinpath[i] = netedge
+end
+
+# find the node that should be the new tip after merging
+newtip = nodesinpath[1]
+while length(getparents(newtip)) > 0 && getparent(newtip) in nodesinpath
+    newtip = getparent(newtip)
+end
+
+
+
+
+
+InPhyNet.updateconstraints!("A", "B", [net], r, Edge(-1), Edge(-1))
+sum(r.map[collect(keys(r.map))[1]] .!== nothing) > 1
 ###############
 
 
