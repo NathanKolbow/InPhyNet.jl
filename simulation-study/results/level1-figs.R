050821c989da7b548feca45055e90a121d14314c
NathanKolbow <nathankolbow@gmail.com>
2024-07-11 12:00:21 -0500

    updated figures
    Former-commit-id: 96f988f9a7b74ac64c7a987fd668d13ea2b99977
    Former-commit-id: 1caf48403f6839071911dc3e508057fd925ea9cf

--- simulation-study/results/level1-figs.R
+++ simulation-study/results/level1-figs.R
@@ -5,7 +5,7 @@ source("simulation-study/results/level1-helpers.R")
 
 # plot_hwcd_heatmap("n200r10", only_subset_size = 25)
 
-(df_level1 %>% filter(numtaxa == 1000) %>% select(nretics_true))[,1] %>% unique() %>% sort()
+(df_level1 %>% filter(numtaxa == 2500) %>% select(nretics_true))[,1] %>% unique() %>% sort()
 
 # n2500
 plot_hwcd_heatmap_single_l1(2500, 25)
@@ -56,8 +56,8 @@ plot_est_retics_l1(500)
 # SVGs
 fig_dir <- "/mnt/dv/wid/projects4/SolisLemus-network-merging/simulation-study/results/level1-pdfs/"
 
-svg(paste0(fig_dir, "hwcd_m_n500.svg"), width=16, height=9)
-plot_hwcd_heatmap_m_l1(500, width = 6, height = 8)
+svg(paste0(fig_dir, "hwcd_m_n500.svg"), width=16, height=6)
+plot_hwcd_heatmap_m_l1(500, width = 6, height = 8, ms = c(5, 15, 25))
 dev.off()
 
 svg(paste0(fig_dir, "retics_est_n500.svg"), width=12, height=8)
@@ -72,3 +72,10 @@ svg(paste0(fig_dir, "hwcd_n1000_retics.svg"), width=12, height=8)
 plot_hwcd_heatmap_quad_l1(1000, c(8, 15, 23, 50), 25, height=12, width=10)
 dev.off()
 
+svg(paste0(fig_dir, "hwcd_n2500.svg"), width = 8, height = 6)
+plot_hwcd_heatmap_single_l1(2500, 25, width = 10, height = 16)
+dev.off()
+
+svg(paste0(fig_dir, "hwcd_n1000.svg"), width = 8, height = 6)
+plot_hwcd_heatmap_single_l1(1000, 25, width = 10, height = 10)
+dev.off()
