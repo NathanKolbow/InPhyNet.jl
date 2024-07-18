49da55fa76cc84f0c9f2dbb500ec2b1cae21d158
NathanKolbow <nathankolbow@gmail.com>
2024-07-18 08:44:58 -0500

    removed Clustering
    Former-commit-id: 64617b601e0a77153ffd321432f3d5744a784f25 [formerly d8b0ceab56bc37e6c3508007ff779b3ddf251da3] [formerly 1a1d6be0281ef120c6ff97fcf4429bc5a95b17dd] [formerly 14bd43ef54bd7238dacf5f433bc224ebd0c3a17f [formerly a238cd29c06b70f37fc24759de7cf9e8bc3aac75]] [formerly bc3739cd81c07b79610eeb9537bb06bcb19f1346 [formerly 524312e27a50b18ad73cf5752d2b9b5807b99068]]
    Former-commit-id: 0cc68f5528a41301a548a9249bd1355a16ec89a0
    Former-commit-id: 568372b6429db341465a005f684e77ba02ae3977 [formerly b8f9c5e1398362098834d027991b37418a6c258a]
    Former-commit-id: 19a9b7306243119c150b3adfb591e06a6c805f37 [formerly 16982503fea99368637d00d964e2d8159e6dabcd]
    Former-commit-id: d6c58765be91b9ac3b33263e048461952007e4af
    Former-commit-id: 3496c8e313075fda4bcafd08ea45c2b5756b5b77

--- src/imports.jl
+++ src/imports.jl
@@ -1,8 +1,7 @@
-using PhyloNetworks, DataStructures, DataFrames, CSV, Graphs, StatsBase, Clustering
+using PhyloNetworks, DataStructures, DataFrames, CSV, Graphs, StatsBase
 
 import PhyloNetworks: deleteNode!, deleteEdge!, addhybridedge!, fuseedgesat!, getChild
-import Combinatorics: combinations
+import Combinatorics: combinations, partitions, powerset
 import Base: ==, *, -, names, getindex, setindex!
-import Combinatorics: partitions, powerset
 import Test: @warn
 import Graphs: SimpleGraph, a_star, add_edge!
