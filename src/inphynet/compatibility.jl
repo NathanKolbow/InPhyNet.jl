893916c0407e7c46bb19f8ebf1aefb40342d5820
NathanKolbow <nathankolbow@gmail.com>
2024-08-19 09:57:47 -0500

    added inphynet heuristics and heuristic tests
    Former-commit-id: 7e5fd2e34bb30df120da9989619272ad544dbbec [formerly 0eec0ac6c6879c6e561aaa316face13e12e8876f] [formerly 9e2c7751383808d6f3c2bf1360ccfb3471206e9d] [formerly 0598b7cedd7df7c632aa883d9091f4f69c8039fb [formerly 57e669bf1c97bfb4a7f00ab5b697d5a8f59e93fd]] [formerly 931528f89f7fd6fc0f7fab75a535c016b72d1e93 [formerly 04728f60210bafd733f02657551301fe341be987]]
    Former-commit-id: 8d675e3f8be2a5a421114940db1472ab479cf51f
    Former-commit-id: 21c2c7fa9bedfdadeb7878a100fc8b9c7e6ee26e [formerly 9e4d62d726a95a7616db676702955f84ee98b788]
    Former-commit-id: 7cd9281003825cc1dd7388182c7c1b2fe328a59e [formerly 6a40bc6046463455302bf2c0234826141ad6dd57]
    Former-commit-id: 1ed459e11e29106ec7c9077c5b7c104e125c3988
    Former-commit-id: 75c8551dde94fce556d1416189f6d0d9a8d58ed7

---
+++ src/inphynet/compatibility.jl
@@ -0,0 +1,78 @@
+using PhyloNetworks
+
+"""
+Heuristic for determining whether the set of networks `ns` are compatible.
+"""
+function are_compatible_heuristic(ns::AbstractVector{<:HybridNetwork})
+    for i = 1:(length(ns) - 1)
+        for j = 2:length(ns)
+            if !are_compatible_heuristic(ns[i], ns[j]) return false end
+        end
+    end
+    return true
+end
+
+"""
+Heuristic for determining whether trees `t1` and `t2` are compatible.
+In the case of networks, `t1` and `t2` are the major trees of some other networks.
+"""
+function are_compatible_heuristic(t1::HybridNetwork, t2::HybridNetwork)
+    leaf_overlap = intersect([leaf.name for leaf in t1.leaf], [leaf.name for leaf in t2.leaf])
+    
+    if length(leaf_overlap) <= 3 return true end
+
+    t1_prime = pruneTruthFromDecomp(t1, leaf_overlap)
+    t2_prime = pruneTruthFromDecomp(t2, leaf_overlap)
+    
+    return hardwiredClusterDistance(t1_prime, t2_prime, false) == 0
+end
+
+
+function are_compatible_after_merge(ns::AbstractVector{HybridNetwork}, nodenamei::AbstractString, nodenamej::AbstractString)
+
+    for i = 1:(length(ns) - 1)
+        i_leaves = [leaf.name for leaf in ns[i].leaf]
+        if length(i_leaves) <= 3 continue end
+        i_matches = (nodenamei in i_leaves) + (nodenamej in i_leaves)
+        if i_matches == 0 continue end
+
+        # Merge the nodes
+        ni_prime = deepcopy(ns[i])
+        if i_matches == 1
+            nodej_match = findfirst(n -> n == nodenamej, i_leaves)
+            if nodej_match !== nothing
+                ni_prime.leaf[nodej_match].name = nodenamei
+                i_leaves[nodej_match] = nodenamei
+            end
+        end
+
+        for j = (i+1):length(ns)
+            j_leaves = [leaf.name for leaf in ns[j].leaf]
+            if length(j_leaves) <= 3 continue end
+            j_matches = (nodenamei in j_leaves) + (nodenamej in j_leaves)
+            if j_matches == 0 continue end
+            
+            # "Merge" the nodes
+            nj_prime = deepcopy(ns[j])
+            if j_matches == 1
+                nodej_match = findfirst(n -> n == nodenamej, j_leaves)
+                if nodej_match !== nothing
+                    nj_prime.leaf[nodej_match].name = nodenamei
+                    j_leaves[nodej_match] = nodenamei
+                end
+            end
+
+            leaf_overlap = intersect(i_leaves, j_leaves)
+            nodej_match = findfirst(n -> n == nodenamej, leaf_overlap)
+            if nodej_match !== nothing deleteat!(leaf_overlap, nodej_match) end
+
+            if hardwiredClusterDistance(
+                pruneTruthFromDecomp(ni_prime, leaf_overlap),
+                pruneTruthFromDecomp(nj_prime, leaf_overlap),
+                false) > 0 return false end
+        end
+    end
+
+    return true
+end
+are_compatible_after_merge(n1::HybridNetwork, n2::HybridNetwork, nodenamei::AbstractString, nodenamej::AbstractString) = are_compatible_after_merge([n1, n2], nodenamei, nodenamej)
