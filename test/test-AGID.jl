8be0185e45d0e95ee77580006ef4c59b5eebcf56
Nathan Kolbow <nkolbow@wisc.edu>
2025-03-20 16:06:52 -0500

    removed NamedArrays dependency from this test
    Former-commit-id: 1cc78ad5f07a0e5b8177a1d0866815a98f406690 [formerly b7b7a9d82c91223b1a16dd6a36d4b29121e39b73]
    Former-commit-id: 5d197ab83fd5c055fde5e67ca1490d462f17bf64 [formerly 43b150beaaad4411ed485b22cbb2fcfc3fe48d87]
    Former-commit-id: 498ae85fc2017a855ad99887729ab49c9bae1009
    Former-commit-id: 599f14f5cea70bd4ef12cae4b8ab494f54e91a27

--- test/test-AGID.jl
+++ test/test-AGID.jl
@@ -1,6 +1,4 @@
-using InPhyNet, PhyloNetworks, NamedArrays, Test, Logging
-
-Logging.disable_logging(Logging.Warn)
+using InPhyNet, PhyloNetworks, Test
 
 gt1 = readnewick("((A:1,B:1):1,(C:1,D:1):1);")
 gt2 = readnewick("((A:2,B:2):1,(C:2,D:2):1);")
@@ -9,34 +7,34 @@ gt3 = readnewick("(E:1,F:1):1;")
 @testset "internodedistance" begin
     # Test single gene tree calls
     D, namelist = internodedistance(gt1)
-    n = NamedArray(D, (namelist, namelist))
+    @test all(j -> namelist[j] == sort(tiplabels(gt1))[j], 1:length(namelist))
 
-    @test n["A", "B"] == 2
-    @test n["C", "D"] == 2
-    @test n["A", "C"] == 4
-    @test n["A", "D"] == 4
-    @test n["B", "C"] == 4
-    @test n["B", "D"] == 4
+    @test D[1, 2] == 2
+    @test D[3, 4] == 2
+    @test D[1, 3] == 4
+    @test D[1, 4] == 4
+    @test D[2, 3] == 4
+    @test D[2, 4] == 4
 
 
     D, namelist = internodedistance(gt2)
-    n = NamedArray(D, (namelist, namelist))
+    @test all(j -> namelist[j] == sort(tiplabels(gt2))[j], 1:length(namelist))
 
-    @test n["A", "B"] == 4
-    @test n["C", "D"] == 4
-    @test n["A", "C"] == 6
-    @test n["A", "D"] == 6
-    @test n["B", "C"] == 6
-    @test n["B", "D"] == 6
+    @test n[1, 2] == 4
+    @test n[3, 4] == 4
+    @test n[1, 3] == 6
+    @test n[1, 4] == 6
+    @test n[2, 3] == 6
+    @test n[2, 4] == 6
 end
 
 @testset "AGID - missing pairs" begin
     D, namelist = calculateAGID([gt1, gt3], allow_missing_pairs=true, default_missing_value=Inf)
-    n = NamedArray(D, (namelist, namelist))
+    @test all(j -> namelist[j] == sort(vcat(tiplabels(gt1), tiplabels(gt2)))[j], 1:length(namelist))
 
-    @test n["A", "E"] == Inf
-    @test n["E", "F"] == 2
-    @test n["A", "B"] == 2
+    @test n[1, 5] == Inf
+    @test n[5, 6] == 2
+    @test n[1, 2] == 2
 end
 
 
@@ -51,10 +49,10 @@ end
 
     
     D, namelist = calculateAGIC([gt1, gt3], allow_missing_pairs=true, default_missing_value=Inf)
-    n = NamedArray(D, (namelist, namelist))
+    @test all(j -> namelist[j] == sort(vcat(tiplabels(gt1), tiplabels(gt3)))[j], 1:length(namelist))
 
-    @test n["A", "E"] == Inf
-    @test n["A", "B"] == 1
-    @test n["E", "F"] == 0  # 0 instead of 1 b/c the tree is treated as unrooted
+    @test n[1, 5] == Inf
+    @test n[1, 2] == 1
+    @test n[5, 6] == 0  # 0 instead of 1 b/c the tree is treated as unrooted
 end
 
