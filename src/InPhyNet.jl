989d896555d815d813b3bced2914df84b95b449b
Nathan Kolbow <nkolbow@wisc.edu>
2025-03-20 16:06:40 -0500

    removed old file includes

--- src/InPhyNet.jl
+++ src/InPhyNet.jl
@@ -10,11 +10,9 @@ module InPhyNet
     include("inphynet/structs/SubNet.jl")
     include("inphynet/structs/ReticMap.jl")
     
-    include("inphynet/neighbors.jl")
     include("inphynet/compatibility.jl")
     include("inphynet/inphynet.jl")
     include("inphynet/internodedistance.jl")
-    include("inphynet/mscquartetsinterface.jl")
     include("inphynet/subsetdecomp.jl")
     include("inphynet/subsetdecomp/pruning.jl")
     include("inphynet/subsetdecomp/satedecomp.jl")
@@ -34,24 +32,5 @@ module InPhyNet
         are_compatible_heuristic, are_compatible_after_merge,
         # DOCS WALKTHROUGH DATA LOADING FXNS
         load_inphynet_example_gts
-
-# include("ptrees/structs/InterimParentalTree.jl")
-# include("ptrees/structs/Lineage.jl")
-# include("ptrees/structs/LineageNode.jl")
-
-# include("ptrees/proba.jl")
-# include("ptrees/coalescent.jl")
-# include("ptrees/arbitrary-proba.jl")
-# include("ptrees/ParentalTrees.jl")
-# include("ptrees/ManipHelpers.jl")
-
-# export Lineage,
-#     LineageNode,
-#     InterimParentalTree, IPT,
-#     nlineages,
-#     lineages,
-#     getParentalTrees,
-#     ntaxa,
-#     coalesce
 
 end
