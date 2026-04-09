2a06f6d8948cad376bad74590d79958748c47b1c
Nathan Kolbow <nkolbow@wisc.edu>
2026-04-09 17:07:13 -0500

    added docstring for loadconflictexample()

--- src/data_examples.jl
+++ src/data_examples.jl
@@ -8,7 +8,13 @@ function load_inphynet_example_gts()::Vector{HybridNetwork}
 
 end
 
-function loadconflictexample()
+
+"""
+Loads the distance matrix `D`, namelist `namelist`, and constraint networks `constraints`
+necessary to reproduce the merge conflict example shown in the Appendices of the InPhyNet
+manuscript. Further details are provided upon executing the function in the terminal.
+"""
+function loadconflictexample()::Nothing
 	example_dir = joinpath(dirname(pathof(InPhyNet)), "..", "examples", "app5-merge-conflict")
 	D_lines = readlines(joinpath(example_dir, "D.csv"))
 	namelist = String.(split(D_lines[1], ","))
@@ -17,5 +23,14 @@ function loadconflictexample()
 		D[i, :] .= [parse(Float64, value) for value in split(D_lines[i+1], ",")]
 	end
 	constraints = readmultinewick(joinpath(example_dir, "constraints.net"))
-	return D, namelist, constraints
+    Core.eval(Main, :(D = $D))
+    Core.eval(Main, :(constraints = $constraints))
+    Core.eval(Main, :(namelist = $namelist))
+
+    printstyled("\n\tExample data has been loaded into the variables `D`, `namelist`, and `constraints`. To see the conflict, paste the following into the terminal:\n\n", color=:black)
+    printstyled("\t\tinphynet(D, constraints, namelist; refuse_pairwise=true)", color=:cyan)
+    printstyled("\n\n\tThis will throw an error stating \"No compatible merge found.\" To use the pairwise version of InPhyNet as a fallback, run the `inphynet` function as usual:\n\n", color=:black)
+    printstyled("\t\tinphynet(D, constraints, namelist)", color=:cyan)
+    printstyled("\n", color=:black)
+    return nothing
 end
